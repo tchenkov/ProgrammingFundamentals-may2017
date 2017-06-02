39cf015c2902218e8facae4e371daea297e3bdfb
Todor Chenkov <joto_to@mail.bg>
2017-06-03 00:49:51 +0300

    -add P01_PracticeIntegers -add exercises conditions

---
+++ L07_DataTypesandVariables-Exercises/P01_PracticeIntegers/P01_PracticeIntegers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P01_PracticeIntegers
+{
+    class P01_PracticeIntegers
+    {
+        static void Main(string[] args)
+        {
+            var num1 = sbyte.Parse(Console.ReadLine());
+            var num2 = byte.Parse(Console.ReadLine());
+            var num3 = short.Parse(Console.ReadLine());
+            var num4 = ushort.Parse(Console.ReadLine());
+            var num5 = uint.Parse(Console.ReadLine());
+            var num6 = int.Parse(Console.ReadLine());
+            var num7 = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+            Console.WriteLine(num4);
+            Console.WriteLine(num5);
+            Console.WriteLine(num6);
+            Console.WriteLine(num7);
+        }
+    }
+}
