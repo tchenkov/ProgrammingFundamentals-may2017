6f9b7a310486d613ecc382f921051772242b25a2
Todor Chenkov <joto_to@mail.bg>
2017-06-02 23:44:22 +0300

    -add P06_TriplesOfLetters

---
+++ L06_Data Types and Variables - Lab/P06_TriplesOfLetters/P06_TriplesOfLetters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P06_TriplesOfLetters
+{
+    class P06_TriplesOfLetters
+    {
+        static void Main(string[] args)
+        {
+            int lettersCount = int.Parse(Console.ReadLine()) + 'a';
+
+            for (char i = 'a'; i < lettersCount; i++)
+            {
+                for (char j = 'a'; j < lettersCount; j++)
+                {
+                    for (char k = 'a'; k < lettersCount; k++)
+                    {
+                        string lettersString = "" + i + j + k;
+                        Console.WriteLine(lettersString);
+                    }
+                }
+            }
+        }
+    }
+}
