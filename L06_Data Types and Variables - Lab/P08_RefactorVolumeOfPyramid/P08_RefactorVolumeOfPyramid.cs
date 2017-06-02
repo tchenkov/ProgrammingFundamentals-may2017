db0d242c27ff9351da7a9e40afd29f319d81e481
Todor Chenkov <joto_to@mail.bg>
2017-06-02 23:57:33 +0300

    -add P08_RefactorVolumeOfPyramid

---
+++ L06_Data Types and Variables - Lab/P08_RefactorVolumeOfPyramid/P08_RefactorVolumeOfPyramid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P08_RefactorVolumeOfPyramid
+{
+    class P08_RefactorVolumeOfPyramid
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double height = double.Parse(Console.ReadLine());
+            double pyramidVolume = (length * width * height) / 3;
+            Console.WriteLine($"Pyramid Volume: {pyramidVolume:F2}");
+        }
+    }
+}
