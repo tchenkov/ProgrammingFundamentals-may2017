6e6bfd6ccd1ad377585d6f37f46e2c67dfbe928b
Todor Chenkov <joto_to@mail.bg>
2017-06-02 23:19:15 +0300

    -add P01_CenturiesToMinutes -update sln file

---
+++ L06_Data Types and Variables - Lab/P01_CenturiesToMinutes/P01_CenturiesToMinutes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01_CenturiesToMinutes
+{
+    class P01_CenturiesToMinutes
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            const double daysPerYear = 365.242;
+            int days = (int)Math.Round(years * daysPerYear);
+            int hours = days * 24;
+            int minutes = hours * 60;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes%n");
+        }
+    }
+}
