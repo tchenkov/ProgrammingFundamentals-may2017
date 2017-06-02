060f533ac404cc4116e67ea62905b8d023892c7a
Todor Chenkov <joto_to@mail.bg>
2017-06-03 00:54:27 +0300

    -add P02_PracticeFloatingPoints

---
+++ L07_DataTypesandVariables-Exercises/P02_PracticeFloatingPoints/P02_PracticeFloatingPoints.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P02_PracticeFloatingPoints
+{
+    class P02_PracticeFloatingPoints
+    {
+        static void Main(string[] args)
+        {
+            var floatNum1 = decimal.Parse(Console.ReadLine());
+            var floatNum2 = double.Parse(Console.ReadLine());
+            var floatNum3 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(floatNum1);
+            Console.WriteLine(floatNum2);
+            Console.WriteLine(floatNum3);
+        }
+    }
+}
