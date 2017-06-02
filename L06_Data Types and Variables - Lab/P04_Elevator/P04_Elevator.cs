e1d712510fef425158b3e877b6ccfbd1ee3b089f
Todor Chenkov <joto_to@mail.bg>
2017-06-02 23:35:49 +0300

    -add P04_Elevator

---
+++ L06_Data Types and Variables - Lab/P04_Elevator/P04_Elevator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P04_Elevator
+{
+    class P04_Elevator
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+                        
+            int elevatorCourses = (int)Math.Ceiling((double)peopleCount / elevatorCapacity);
+            Console.WriteLine(elevatorCourses);
+        }
+    }
+}
