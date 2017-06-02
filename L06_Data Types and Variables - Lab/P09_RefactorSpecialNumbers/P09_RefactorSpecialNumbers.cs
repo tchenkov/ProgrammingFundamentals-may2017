3a4c93d305141a9fbb6ad37d95290a7bcd936ae7
Todor Chenkov <joto_to@mail.bg>
2017-06-03 00:01:45 +0300

    -add P09_RefactorSpecialNumbers

---
+++ L06_Data Types and Variables - Lab/P09_RefactorSpecialNumbers/P09_RefactorSpecialNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P09_RefactorSpecialNumbers
+{
+    class P09_RefactorSpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                int numDigitsSum = SumOfDigits(i);
+
+                bool isNumSpecial = (numDigitsSum == 5) || (numDigitsSum == 7) || (numDigitsSum == 11);
+                Console.WriteLine("{0} -> {1}", i, isNumSpecial);
+            }
+        }
+
+        static int SumOfDigits(int i)
+        {
+            int numDigitsSum = 0;
+            while (i > 0)
+            {
+                numDigitsSum += i % 10;
+                i /= 10;
+            }
+
+            return numDigitsSum;
+        }
+    }
+}
