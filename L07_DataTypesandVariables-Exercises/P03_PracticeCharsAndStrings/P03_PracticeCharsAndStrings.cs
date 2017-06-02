32cc8c335dc445a6d56e7ab12c0a3f0bf290e710
Todor Chenkov <joto_to@mail.bg>
2017-06-03 01:01:15 +0300

    -add P03_PracticeCharsAndStrings

---
+++ L07_DataTypesandVariables-Exercises/P03_PracticeCharsAndStrings/P03_PracticeCharsAndStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P03_PracticeCharsAndStrings
+{
+    class P03_PracticeCharsAndStrings
+    {
+        static void Main(string[] args)
+        {
+            var str1 = Console.ReadLine();
+            var letter1 = char.Parse(Console.ReadLine());
+            var letter2 = char.Parse(Console.ReadLine());
+            var letter3 = char.Parse(Console.ReadLine());
+            var str2 = Console.ReadLine();
+
+            Console.WriteLine(str1);
+            Console.WriteLine(letter1);
+            Console.WriteLine(letter2);
+            Console.WriteLine(letter3);
+            Console.WriteLine(str2);
+        }
+    }
+}
