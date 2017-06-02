9e0626a5e628009d7fd4fdc565ceade4cf9574e2
Todor Chenkov <joto_to@mail.bg>
2017-06-02 23:39:54 +0300

    -add P05_SpecialNumbers

---
+++ L06_Data Types and Variables - Lab/P05_SpecialNumbers/P05_SpecialNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P05_SpecialNumbers
+{
+    class P05_SpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                int currentNumDigitSum = SumOfDigits(i);
+                bool isCurrentNumSpecial = currentNumDigitSum == 5 || currentNumDigitSum == 7 || currentNumDigitSum == 11;
+                Console.WriteLine($"{i} -> {isCurrentNumSpecial}");
+            }
+        }
+
+        static int SumOfDigits(int number)
+        {
+            int sum = 0;
+            do
+            {
+                sum += number % 10;
+                number /= 10;
+            } while (number > 0);
+            return sum;
+        }
+    }
+}
