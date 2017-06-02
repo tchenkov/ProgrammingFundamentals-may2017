8ba6010e3fa30fd03a34df97da35d0109a9d9b43
Todor Chenkov <joto_to@mail.bg>
2017-06-02 23:23:54 +0300

    -add P02_CircleArea

---
+++ L06_Data Types and Variables - Lab/P02_CircleArea/P02_CircleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P02_CircleArea
+{
+    class P02_CircleArea
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double pi = Math.PI;
+            double area = pi * radius * radius;
+            Console.WriteLine(area);
+        }
+    }
+}
