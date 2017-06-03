58ed2f394174ba0ab4dc2f8f969b530e4d401a9b
Todor Chenkov <joto_to@mail.bg>
2017-06-03 22:30:10 +0300

    -add P02_NumberChecker

---
+++ L08_DataTypesAndVariables-MoreExercises/P02_NumberChecker/P02_NumberChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P02_NumberChecker
+{
+    class P02_NumberChecker
+    {
+        static void Main(string[] args)
+        {
+            string numberString = Console.ReadLine();
+            try
+            {
+                long.Parse(numberString);
+                Console.WriteLine("integer");
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    decimal.Parse(numberString);
+                    Console.WriteLine("floating-point");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+    }
+}
