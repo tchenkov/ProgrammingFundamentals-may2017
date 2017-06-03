b89120184356bd8bd94f8ec5e2cf8ff11ed98b46
Todor Chenkov <joto_to@mail.bg>
2017-06-03 22:24:30 +0300

    -add P01_TypeBoundaries

---
+++ L08_DataTypesAndVariables-MoreExercises/P01_TypeBoundaries/P01_TypeBoundaries.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01_TypeBoundaries
+{
+    class P01_TypeBoundaries
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            GetDateTypeBoundaries(dataType);
+        }
+
+        private static void GetDateTypeBoundaries(string dataType)
+        {
+            string minStr = "min";
+            string maxStr = "max";
+            var dataTypesMinMax = new Dictionary<string, Dictionary<string, string>>
+            {
+                {
+                    "sbyte", new Dictionary<string, string>()
+                    {
+                        {minStr, sbyte.MinValue.ToString()}, { maxStr, sbyte.MaxValue.ToString()}
+                    }
+                },
+
+                {
+                    "byte", new Dictionary<string, string>()
+                    {
+                        {minStr, byte.MinValue.ToString()}, {maxStr, byte.MaxValue.ToString()}
+                    }
+                },
+
+                {
+                    "int", new Dictionary<string, string>()
+                    {
+                        {minStr, int.MinValue.ToString()}, {maxStr, int.MaxValue.ToString()}
+                    }
+                },
+
+                {
+                    "uint", new Dictionary<string, string>()
+                    {
+                        {minStr, uint.MinValue.ToString()}, {maxStr, uint.MaxValue.ToString()}
+                    }
+                },
+
+                {
+                    "long", new Dictionary<string, string>()
+                    {
+                        {minStr, long.MinValue.ToString()}, {maxStr, long.MaxValue.ToString()}
+                    }
+                }
+            };
+
+            Console.WriteLine(dataTypesMinMax[dataType][maxStr]);
+            Console.WriteLine(dataTypesMinMax[dataType][minStr]);
+        }
+    }
+}
