5eed8a7b30df765476987f0d8bd7116739eceb3c
Todor Chenkov <joto_to@mail.bg>
2017-06-03 14:49:20 +0300

    -add P10_CenturiesToNanoseconds

---
+++ L07_DataTypesandVariables-Exercises/P10_CenturiesToNanoseconds/P10_CenturiesToNanoseconds.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace P10_CenturiesToNanoseconds
+{
+    class P10_CenturiesToNanoseconds
+    {
+        static void Main(string[] args)
+        {
+            byte centureies = byte.Parse(Console.ReadLine());
+            ushort years = (ushort) (centureies * 100);
+            int days = (int)Math.Round(years * 365.2422);
+            int hours = days * 24;
+            long minutes = (long)hours * 60;
+            long seconds = minutes * 60;
+            ulong milliseconds = (ulong)seconds * 1000;
+            BigInteger microseconds = (BigInteger)milliseconds * 1000;
+            BigInteger nanoseconds = microseconds * 1000;
+
+            Console.WriteLine($"{centureies} centuries = {years} years = {days} days = {hours} hours = {minutes}"+ 
+                $" minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
+        }
+    }
+}
