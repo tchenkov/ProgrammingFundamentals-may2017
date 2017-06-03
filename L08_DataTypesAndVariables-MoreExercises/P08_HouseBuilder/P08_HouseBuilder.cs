6f7d7fa91da48e73f3db231602e7224ffb32b016
Todor Chenkov <joto_to@mail.bg>
2017-06-04 00:20:40 +0300

    -add P08_HouseBuilder

--- L08_DataTypesAndVariables-MoreExercises/P08_HouseBuilder/P08_HouseBuilder.cs
+++ L08_DataTypesAndVariables-MoreExercises/P08_HouseBuilder/P08_HouseBuilder.cs
@@ -6,7 +6,9 @@ namespace P08_HouseBuilder
     {
         static void Main(string[] args)
         {
-
+            long price1 = long.Parse(Console.ReadLine());
+            long price2 = long.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Max(price1, price2) * 10 + Math.Min(price1, price2) * 4);
         }
     }
 }
