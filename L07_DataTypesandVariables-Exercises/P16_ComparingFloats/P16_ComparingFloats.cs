3627bff1d29b98f71cacf766198f2845e362d31c
Todor Chenkov <joto_to@mail.bg>
2017-06-03 16:29:38 +0300

    -add P16_ComparingFloats

---
+++ L07_DataTypesandVariables-Exercises/P16_ComparingFloats/P16_ComparingFloats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P16_ComparingFloats
+{
+    class P16_ComparingFloats
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double difference = Math.Abs(num1 - num2);
+            const double eps = 0.000001;
+            bool areNumsRelativelyEqual = difference < eps;
+            Console.WriteLine(areNumsRelativelyEqual);
+        }
+    }
+}
