cd5bdadcbbef14d56f0fbe7b349a691a952596d4
Todor Chenkov <joto_to@mail.bg>
2017-06-04 01:10:17 +0300

    -add P10_SumOfChars

---
+++ L08_DataTypesAndVariables-MoreExercises/P10_SumOfChars/P10_SumOfChars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P10_SumOfChars
+{
+    class P10_SumOfChars
+    {
+        static void Main(string[] args)
+        {
+            byte charsCount = byte.Parse(Console.ReadLine());
+            int sum = 0;
+            while (charsCount > 0)
+            {
+                sum += char.Parse(Console.ReadLine());
+                charsCount--;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
