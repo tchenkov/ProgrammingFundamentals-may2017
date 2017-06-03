0527b9837c67704a5020d282fe8bdb30cb8551f8
Todor Chenkov <joto_to@mail.bg>
2017-06-03 14:03:02 +0300

    -add P09_ReversedChars

---
+++ L07_DataTypesandVariables-Exercises/P09_ReversedChars/P09_ReversedChars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P09_ReversedChars
+{
+    class P09_ReversedChars
+    {
+        static void Main(string[] args)
+        {
+            var char1 = char.Parse(Console.ReadLine());
+            var char2 = char.Parse(Console.ReadLine());
+            var char3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{char3}{char2}{char1}");
+        }
+    }
+}
