9a461b1ac95dc06b1f7bded137ee88fa8327a905
Todor Chenkov <joto_to@mail.bg>
2017-06-03 16:11:56 +0300

    -add P14_IntegerToHexAndBinary

---
+++ P14_IntegerToHexAndBinary/P14_IntegerToHexAndBinary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P14_IntegerToHexAndBinary
+{
+    class P14_IntegerToHexAndBinary
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(num, 16).ToUpper());
+            Console.WriteLine(Convert.ToString(num, 2));
+        }
+    }
+}
