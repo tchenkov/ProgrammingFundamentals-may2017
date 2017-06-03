5f316e47c8c5f8753b770eb13b63be33c68038c9
Todor Chenkov <joto_to@mail.bg>
2017-06-04 02:23:42 +0300

    -add P13_DecryptingMessage

---
+++ L08_DataTypesAndVariables-MoreExercises/P13_DecryptingMessage/P13_DecryptingMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P13_DecryptingMessage
+{
+    class P13_DecryptingMessage
+    {
+        static void Main(string[] args)
+        {
+            byte decriptionKey = byte.Parse(Console.ReadLine());
+            byte charactersCount = byte.Parse(Console.ReadLine());
+            string message = "";
+
+            while (charactersCount > 0)
+            {
+                message += ((char)(char.Parse(Console.ReadLine()) + decriptionKey)).ToString();
+
+                charactersCount--;
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
