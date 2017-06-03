d1594a2dd99683fa9f47e9b09cda455ab554ec9d
Todor Chenkov <joto_to@mail.bg>
2017-06-03 23:33:56 +0300

    -add P05_WeatherForecast

---
+++ L08_DataTypesAndVariables-MoreExercises/P05_WeatherForecast/P05_WeatherForecast.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace P05_WeatherForecast
+{
+    class P05_WeatherForecast
+    {
+        static void Main(string[] args)
+        {
+            string numberString = Console.ReadLine();
+            try
+            {
+                sbyte.Parse(numberString);
+                Console.WriteLine("Sunny");
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    int.Parse(numberString);
+                    Console.WriteLine("Cloudy");
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        long.Parse(numberString);
+                        Console.WriteLine("Windy");
+                    }
+                    catch (Exception)
+                    {
+                        try
+                        {
+                            decimal.Parse(numberString);
+                            Console.WriteLine("Rainy");
+                        }
+                        catch(Exception)
+                        {
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
