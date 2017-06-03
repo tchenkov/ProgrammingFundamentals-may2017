91f668c705a35f0ef528ebdf93d4c6b011e2cb5b
Todor Chenkov <joto_to@mail.bg>
2017-06-03 12:46:46 +0300

    -add P04_VariableInHexFormat

---
+++ L07_DataTypesandVariables-Exercises/P04_VariableInHexFormat/P04_VariableInHexFormat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P04_VariableInHexFormat
+{
+    class P04_VariableInHexFormat
+    {
+        static void Main(string[] args)
+        {
+            var num = Convert.ToInt32(Console.ReadLine(), 16);
+            Console.WriteLine(num);
+        }
+    }
+}
