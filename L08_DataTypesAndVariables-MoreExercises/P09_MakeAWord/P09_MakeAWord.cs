06aaf25af06272c2cc9a8270d877de149ec27f9b
Todor Chenkov <joto_to@mail.bg>
2017-06-04 01:05:31 +0300

    -add P09_MakeAWord

---
+++ L08_DataTypesAndVariables-MoreExercises/P09_MakeAWord/P09_MakeAWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P09_MakeAWord
+{
+    class P09_MakeAWord
+    {
+        static void Main(string[] args)
+        {
+            byte wordSize = byte.Parse(Console.ReadLine());
+            char[] wordLetters = new char[wordSize];
+
+            for (int i = 0; i < wordSize; i++)
+            {
+                wordLetters[i] = char.Parse(Console.ReadLine());
+            }
+
+            string word = new string(wordLetters);
+            Console.WriteLine($"The word is: {word}");
+        }
+    }
+}
