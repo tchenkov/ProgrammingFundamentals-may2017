fbfe9cd92bc768e52a14567f19a629d79b1214d6
Todor Chenkov <joto_to@mail.bg>
2017-06-03 16:20:29 +0300

    -add P15_FastPrimeChecker -updated some paths

---
+++ L07_DataTypesandVariables-Exercises/P15_FastPrimeChecker/P15_FastPrimeChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P15_FastPrimeChecker
+{
+    class P15_FastPrimeChecker
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            for (int currentNumber = 2; currentNumber <= numbersCount; currentNumber++)
+            {
+                bool isPrime = true;
+                for (int i = 2; i <= Math.Sqrt(currentNumber); i++)
+                {
+                    if (currentNumber % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{currentNumber} -> {isPrime}");
+            }
+
+        }
+    }
+}
