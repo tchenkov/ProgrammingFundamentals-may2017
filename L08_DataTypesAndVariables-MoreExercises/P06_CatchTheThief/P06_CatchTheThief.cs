d939632fc6cbe299627595341e5042549890cd2f
Todor Chenkov <joto_to@mail.bg>
2017-06-03 23:52:18 +0300

    -add P06_CatchTheThief

---
+++ L08_DataTypesAndVariables-MoreExercises/P06_CatchTheThief/P06_CatchTheThief.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace P06_CatchTheThief
+{
+    class P06_CatchTheThief
+    {
+        static void Main(string[] args)
+        {
+            string dataTypeThiefID = Console.ReadLine();
+            byte countOfIDs = byte.Parse(Console.ReadLine());
+            long thiefID = long.MinValue;
+            while (countOfIDs > 0)
+            {
+                string currentID = Console.ReadLine();
+                if (dataTypeThiefID == "sbyte")
+                {
+                    try
+                    {
+                        long currentIDNum = sbyte.Parse(currentID);
+                        thiefID = Math.Max(currentIDNum, thiefID);
+                    }
+                    catch(Exception)
+                    {
+
+                    }
+                }
+                if (dataTypeThiefID == "int")
+                {
+                    try
+                    {
+                        long currentIDNum = int.Parse(currentID);
+                        thiefID = Math.Max(currentIDNum, thiefID);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                if (dataTypeThiefID == "long")
+                {
+                    try
+                    {
+                        long currentIDNum = long.Parse(currentID);
+                        thiefID = Math.Max(currentIDNum, thiefID);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+
+                countOfIDs--;
+            }
+
+            Console.WriteLine(thiefID);
+        }
+    }
+}
