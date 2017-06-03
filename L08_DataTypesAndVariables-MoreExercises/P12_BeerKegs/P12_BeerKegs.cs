eef46b9e64af66e6c85e8047da7ecbe833426ffe
Todor Chenkov <joto_to@mail.bg>
2017-06-04 02:15:24 +0300

    -add P12_BeerKegs

---
+++ L08_DataTypesAndVariables-MoreExercises/P12_BeerKegs/P12_BeerKegs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P12_BeerKegs
+{
+    class P12_BeerKegs
+    {
+        static void Main(string[] args)
+        {
+            byte kegsCount = byte.Parse(Console.ReadLine());
+            string biggestKegModel = "";
+            double biggestKegVolume = 0;
+            float pi = (float)Math.PI;
+
+            while (kegsCount > 0)
+            {
+                string currentKegModel = Console.ReadLine();
+                float kegRadius = float.Parse(Console.ReadLine());
+                int kegHeight = int.Parse(Console.ReadLine());
+                double currentVolume = (double)pi * kegRadius * kegRadius * kegHeight;
+                if (currentVolume > biggestKegVolume)
+                {
+                    biggestKegVolume = currentVolume;
+                    biggestKegModel = currentKegModel;
+                }
+                kegsCount--;
+            }
+
+            Console.WriteLine(biggestKegModel);
+        }
+    }
+}
