80268b0f0968ca26bf147a9be904fed222eb18c1
Todor Chenkov <joto_to@mail.bg>
2017-06-03 15:31:04 +0300

    -add P11_ConvertSpeedUnits

---
+++ L07_DataTypesandVariables-Exercises/P11_ConvertSpeedUnits/P11_ConvertSpeedUnits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P11_ConvertSpeedUnits
+{
+    class P11_ConvertSpeedUnits
+    {
+        static void Main(string[] args)
+        {
+            float distanceInMeters = float.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int seconds = int.Parse(Console.ReadLine());
+
+            int totalTimeInSeconds = (hours * 60 + minutes) * 60 + seconds;
+
+            float speedMPSec = distanceInMeters / totalTimeInSeconds;
+            Console.WriteLine(speedMPSec);
+
+            float distanceInKM = distanceInMeters / 1000;
+            float totalTimeInHours = totalTimeInSeconds / 3600.0f;
+            float speedKPHours = distanceInKM / totalTimeInHours;
+            Console.WriteLine(speedKPHours);
+
+            float distanceInMiles = distanceInMeters / 1609;
+            float speedInMilesPerHour = distanceInMiles / totalTimeInHours;
+            Console.WriteLine(speedInMilesPerHour);
+        }
+    }
+}
