049af566aef6bacd9bb3c00cd8df05b2e064a6f7
Todor Chenkov <joto_to@mail.bg>
2017-06-03 22:56:13 +0300

    -add P03_WaterOverflow

---
+++ L08_DataTypesAndVariables-MoreExercises/P03_WaterOverflow/P03_WaterOverflow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P03_WaterOverflow
+{
+    class P03_WaterOverflow
+    {
+        static void Main(string[] args)
+        {
+            byte pipesCount = byte.Parse(Console.ReadLine());
+            byte tankCapacity = byte.MaxValue;
+            byte currentTankVolume = 0;
+            while (pipesCount > 0)
+            {
+                short waterVolume = short.Parse(Console.ReadLine());
+                if (currentTankVolume + waterVolume <= tankCapacity)
+                {
+                    currentTankVolume += (byte)waterVolume;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                pipesCount--;
+            }
+
+            Console.WriteLine(currentTankVolume);
+        }
+    }
+}
