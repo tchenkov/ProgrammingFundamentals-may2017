af321c9ff06844f03567465df77a23ffeb99f5c1
Todor Chenkov <joto_to@mail.bg>
2017-06-04 01:47:47 +0300

    -add P11_StringConcatenation

---
+++ L08_DataTypesAndVariables-MoreExercises/P11_StringConcatenation/P11_StringConcatenation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P11_StringConcatenation
+{
+    class P11_StringConcatenation
+    {
+        static void Main(string[] args)
+        {
+            string separator = Console.ReadLine();
+            byte remainder = Console.ReadLine() == "even" ? (byte)0 : (byte)1;
+            byte stringsCount = byte.Parse(Console.ReadLine());
+            string concatenatedString = "";
+
+            for (int i = 1; i <= stringsCount; i++)
+            {
+                if (i % 2 == remainder)
+                {
+                    concatenatedString += Console.ReadLine() + separator;
+                }
+                else
+                {
+                    Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine(concatenatedString.Remove(concatenatedString.Length-1));
+        }
+    }
+}
