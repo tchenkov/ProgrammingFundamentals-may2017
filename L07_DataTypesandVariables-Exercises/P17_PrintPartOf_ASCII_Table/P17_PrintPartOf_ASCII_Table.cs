af6acab0be4bfb9a47733c0a592d7b7e88b836fc
Todor Chenkov <joto_to@mail.bg>
2017-06-03 17:19:49 +0300

    -add P17_PrintPartOf_ASCII_Table

---
+++ L07_DataTypesandVariables-Exercises/P17_PrintPartOf_ASCII_Table/P17_PrintPartOf_ASCII_Table.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P17_PrintPartOf_ASCII_Table
+{
+    class P17_PrintPartOf_ASCII_Table
+    {
+        static void Main(string[] args)
+        {
+            char startChar = (char) int.Parse(Console.ReadLine());
+            char endChar = (char)int.Parse(Console.ReadLine());
+            OutputCharInterval(startChar, endChar);
+        }
+
+        static void OutputCharInterval(char startChar, char endChar)
+        {
+            if (startChar < endChar)
+            {
+                Console.Write($"{startChar} ");
+                OutputCharInterval((char)(startChar + 1), endChar);
+            }
+            else
+            {
+                Console.WriteLine($"{startChar}");
+            }
+        }
+    }
+}
