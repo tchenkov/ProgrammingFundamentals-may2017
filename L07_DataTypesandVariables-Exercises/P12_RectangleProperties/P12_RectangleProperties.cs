c706c0856e3a7aa9d29abe0ed295d3ad29215f04
Todor Chenkov <joto_to@mail.bg>
2017-06-03 15:37:05 +0300

    -add P12_RectangleProperties

---
+++ L07_DataTypesandVariables-Exercises/P12_RectangleProperties/P12_RectangleProperties.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P12_RectangleProperties
+{
+    class P12_RectangleProperties
+    {
+        static void Main(string[] args)
+        {
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+
+            double rectanglePerimeter = (sideA + sideB) * 2;
+            Console.WriteLine(rectanglePerimeter);
+
+            double rectangleArea = sideA * sideB;
+            Console.WriteLine(rectangleArea);
+
+            double rectangleDiagonal = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+            Console.WriteLine(rectangleDiagonal);
+        }
+    }
+}
