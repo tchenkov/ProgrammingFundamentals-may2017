a0e43fbc5f993221c240c81cbf98d4f270db9f57
Todor Chenkov <joto_to@mail.bg>
2017-06-03 21:34:32 +0300

    -add P19_TheaThePhotographer

---
+++ L07_DataTypesandVariables-Exercises/P19_TheaThePhotographer/P19_TheaThePhotographer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P19_TheaThePhotographer
+{
+    class P19_TheaThePhotographer
+    {
+        static void Main(string[] args)
+        {
+            long pircturesCount = int.Parse(Console.ReadLine());
+            int filterTime = int.Parse(Console.ReadLine());
+            byte filterFactor = byte.Parse(Console.ReadLine());
+            long uploadTime = int.Parse(Console.ReadLine());
+
+            long timeToFilterAllPics = pircturesCount * filterTime;
+            int filteredPicsCount = (int) Math.Ceiling(pircturesCount * filterFactor / 100.0);
+            long timeToUpload = filteredPicsCount * uploadTime;
+            long readyTime = timeToFilterAllPics + timeToUpload;
+            byte seconds = (byte) (readyTime % 60);
+            readyTime = (readyTime - (readyTime % 60)) /60;
+            byte minutes = (byte)(readyTime % 60);
+            readyTime = (readyTime - (readyTime % 60)) / 60;
+            byte hours = (byte)(readyTime % 24);
+            readyTime = (readyTime - (readyTime % 24)) / 24;
+            //int days = readyTime;
+
+            Console.WriteLine($"{readyTime}:{hours:d2}:{minutes:d2}:{seconds:d2}");
+        }
+    }
+}
