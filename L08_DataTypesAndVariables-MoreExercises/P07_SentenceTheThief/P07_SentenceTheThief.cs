548a75aabb1c0e85e51efb58baf6762eb6c0b501
Todor Chenkov <joto_to@mail.bg>
2017-06-04 00:10:46 +0300

    -add P07_SentenceTheThief

---
+++ L08_DataTypesAndVariables-MoreExercises/P07_SentenceTheThief/P07_SentenceTheThief.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace P07_SentenceTheThief
+{
+    class P07_SentenceTheThief
+    {
+        static void Main(string[] args)
+        {
+            string dataTypeThiefID = Console.ReadLine();
+            byte countOfIDs = byte.Parse(Console.ReadLine());
+            long thiefID = long.MinValue;
+            while (countOfIDs > 0)
+            {
+                string currentID = Console.ReadLine();
+                if (dataTypeThiefID == "sbyte")
+                {
+                    try
+                    {
+                        long currentIDNum = sbyte.Parse(currentID);
+                        thiefID = Math.Max(currentIDNum, thiefID);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                if (dataTypeThiefID == "int")
+                {
+                    try
+                    {
+                        long currentIDNum = int.Parse(currentID);
+                        thiefID = Math.Max(currentIDNum, thiefID);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                if (dataTypeThiefID == "long")
+                {
+                    try
+                    {
+                        long currentIDNum = long.Parse(currentID);
+                        thiefID = Math.Max(currentIDNum, thiefID);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+
+                countOfIDs--;
+            }
+
+            if (thiefID > 0)
+            {
+                long sentence = (long) Math.Ceiling((decimal)thiefID / sbyte.MaxValue);
+                Console.WriteLine($"Prisoner with id {thiefID} is sentenced to {sentence} " + (sentence > 1 ? "years" : $"year"));
+            }
+            else
+            {
+                long sentence = (long)Math.Ceiling((decimal)thiefID / sbyte.MinValue);
+                Console.WriteLine($"Prisoner with id {thiefID} is sentenced to {sentence} " + (sentence > 1 ? "years" : $"year"));
+            }
+        }
+    }
+}
---
+++ L08_DataTypesAndVariables-MoreExercises/P08_HouseBuilder/P08_HouseBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P08_HouseBuilder
+{
+    class P08_HouseBuilder
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
