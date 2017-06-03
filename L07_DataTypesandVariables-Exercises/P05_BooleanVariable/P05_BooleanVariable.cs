7d90d47f06b4408e47915a0467fcfb128b686833
Todor Chenkov <joto_to@mail.bg>
2017-06-03 12:52:01 +0300

    -add P05_BooleanVariable

---
+++ L07_DataTypesandVariables-Exercises/P05_BooleanVariable/P05_BooleanVariable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P05_BooleanVariable
+{
+    class P05_BooleanVariable
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+            if (isTrue)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
