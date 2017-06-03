e10a4bc0153d961e516c8a894a324b3baea4cfd7
Todor Chenkov <joto_to@mail.bg>
2017-06-03 15:46:45 +0300

    -add P13_VowelOrDigit

---
+++ P13_VowelOrDigit/P13_VowelOrDigit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P13_VowelOrDigit
+{
+    class P13_VowelOrDigit
+    {
+        static void Main(string[] args)
+        {
+            var symbol = Console.ReadLine();
+            try
+            {
+                int.Parse(symbol);
+                Console.WriteLine("digit");
+            }
+            catch (Exception)
+            {
+                char letter = char.Parse(symbol);
+                bool isLetterAVowel =
+                    letter == 'a' ||
+                    letter == 'o' ||
+                    letter == 'u' ||
+                    letter == 'e' ||
+                    letter == 'i';
+                Console.WriteLine(isLetterAVowel ? "vowel" : "other");
+            }
+        }
+    }
+}
