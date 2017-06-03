24d3793d64191a2c6adfc77869ae32ae872a1d55
Todor Chenkov <joto_to@mail.bg>
2017-06-03 13:57:26 +0300

    -update paths

---
+++ L07_DataTypesandVariables-Exercises/P06_StringsAndObjects/P06_StringsAndObjects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P06_StringsAndObjects
+{
+    class P06_StringsAndObjects
+    {
+        static void Main(string[] args)
+        {
+            var string1 = Console.ReadLine();
+            var string2 = Console.ReadLine();
+            string concatenated = String.Concat(string1, " ", string2);
+            Console.WriteLine(concatenated);
+        }
+    }
+}
---
+++ L07_DataTypesandVariables-Exercises/P07_ExchangeVariableValues/P07_ExchangeVariableValues.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P07_ExchangeVariableValues
+{
+    class P07_ExchangeVariableValues
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            bool isBefore = true;
+            OutputTwoNumbers(num1, num2, isBefore);
+            SwapIntValues(ref num1, ref num2, ref isBefore);
+            OutputTwoNumbers(num1, num2, isBefore);
+        }
+
+        static void OutputTwoNumbers(int num1, int num2, bool isBefore)
+        {
+            Console.WriteLine(isBefore ? "Before:" : "After:");
+            Console.WriteLine($"a = {num1}");
+            Console.WriteLine($"b = {num2}");
+        }
+
+        static void SwapIntValues (ref int num1, ref int num2, ref bool isBefore)
+        {
+            isBefore = !isBefore;
+            int memoryNum = num1;
+            num1 = num2;
+            num2 = memoryNum;
+        }
+    }
+}
