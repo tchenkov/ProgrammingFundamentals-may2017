1346fdbe40f3d95855f38141d7b3088e4e895c9b
Todor Chenkov <joto_to@mail.bg>
2017-06-04 03:33:34 +0300

    -add P14_BoatSimulator

---
+++ L08_DataTypesAndVariables-MoreExercises/P14_BoatSimulator/P14_BoatSimulator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace P14_BoatSimulator
+{
+    class P14_BoatSimulator
+    {
+        static void Main(string[] args)
+        {
+            char boat1 = char.Parse(Console.ReadLine());
+            char boat2 = char.Parse(Console.ReadLine());
+            byte numberOfLines = byte.Parse(Console.ReadLine());
+            byte positionBoat1 = 0;
+            byte positionBoat2 = 0;
+
+            for (int i = 1; i <= numberOfLines; i++)
+            {
+                string currentCommand = Console.ReadLine();
+                if (currentCommand == "UPGRADE")
+                {
+                    boat1 += (char)3;
+                    boat2 += (char)3;
+                }
+                else
+                {
+                    if (i % 2 == 1)
+                    {
+                        positionBoat1 += (byte)currentCommand.Length;
+                        if (positionBoat1 >= 50)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        positionBoat2 += (byte)currentCommand.Length;
+                        if (positionBoat2 >= 50)
+                        {
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine(positionBoat1 > positionBoat2 ? boat1 : boat2);
+        }
+    }
+}
