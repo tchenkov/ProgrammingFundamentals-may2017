fd52f5667b5aa3afb9b011b1acfab5bbb12ee28f
Todor Chenkov <joto_to@mail.bg>
2017-06-05 23:06:15 +0300

    -add P01_BlankReceipt

---
+++ L09_MethodsDebuggingAndTroubleshootingCode-Lab/P01_BlankReceipt/P01_BlankReceipt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_BlankReceipt
+{
+    class P01_BlankReceipt
+    {
+        static void Main(string[] args)
+        {
+            blankReceipt();
+        }
+
+        static void blankReceipt()
+        {
+            receiptHeader();
+            receiptBody();
+            receiptFooter();
+        }
+
+        static void receiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+
+        static void receiptBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+        static void receiptFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+    }
+}
---
+++ L09_MethodsDebuggingAndTroubleshootingCode-Lab/P02_SignOfIntegerNumber/P02_SignOfIntegerNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_SignOfIntegerNumber
+{
+    class P02_SignOfIntegerNumber
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
