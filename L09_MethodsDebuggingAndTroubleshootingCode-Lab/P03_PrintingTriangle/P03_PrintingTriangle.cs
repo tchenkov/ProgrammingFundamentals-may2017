27f6d577ccb9c73960241a8e010ec103d31ea7c6
Todor Chenkov <joto_to@mail.bg>
2017-06-05 23:27:58 +0300

    -add P03_PrintingTriangle

---
+++ L09_MethodsDebuggingAndTroubleshootingCode-Lab/P03_PrintingTriangle/P03_PrintingTriangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P03_PrintingTriangle
+{
+    class P03_PrintingTriangle
+    {
+        static void Main(string[] args)
+        {
+            int triangleWidth = int.Parse(Console.ReadLine());
+            PrintTriangleOfNumbers(triangleWidth);
+        }
+
+        static void PrintTriangleOfNumbers(int triangleWidth)
+        {
+            for (int i = 1; i <= triangleWidth; i++)
+            {
+                Console.WriteLine(PrintTriangleLine(1, i));
+            }
+            for (int i = triangleWidth - 1; i >= 1; i--)
+            {
+                Console.WriteLine(PrintTriangleLine(1, i));
+            }
+        }
+
+        static string PrintTriangleLine(int start, int end)
+        {
+            string result = string.Empty;
+            for (int i = start; i <= end; i++)
+            {
+                result += i + " ";
+            }
+            return result;
+        }
+    }
+}
