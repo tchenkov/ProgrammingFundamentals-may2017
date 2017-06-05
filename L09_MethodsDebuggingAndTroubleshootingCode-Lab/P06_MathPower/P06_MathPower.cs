6b18cec340d54823192e01656705dc43039e67fd
Todor Chenkov <joto_to@mail.bg>
2017-06-06 00:05:34 +0300

    -add P06_MathPower

---
+++ L09_MethodsDebuggingAndTroubleshootingCode-Lab/P06_MathPower/P06_MathPower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P06_MathPower
+{
+    class P06_MathPower
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetNumberPowerOf(number, power));
+        }
+
+        static double GetNumberPowerOf(double number, int power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+
+            return result;
+        }
+    }
+}
