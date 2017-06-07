f19c99b9e605d1d785537e8111393c7acb1f6272
Todor Chenkov <joto_to@mail.bg>
2017-06-07 22:25:18 +0300

    -add P09_HolidaysBetweenTwoDates

---
+++ L09_MethodsDebuggingAndTroubleshootingCode-Lab/P09_HolidaysBetweenTwoDates/P09_HolidaysBetweenTwoDates.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace P09_HolidaysBetweenTwoDates
+{
+    class P09_HolidaysBetweenTwoDates
+    {
+        static void Main(string[] args)
+        {
+            string dateFormatString = "d.M.yyyy";
+            var startDate = DateTime.ParseExact(
+                Console.ReadLine(),
+                dateFormatString,
+                CultureInfo.InvariantCulture
+                );
+            var endDate = DateTime.ParseExact(
+                Console.ReadLine(),
+                dateFormatString,
+                CultureInfo.InvariantCulture
+                );
+            var holidaysCount = 0;
+            startDate = startDate.AddDays(
+                (int)startDate.DayOfWeek >= 1 ?
+                6 - (int)startDate.DayOfWeek :
+                0
+                );
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    holidaysCount++;
+                }
+                if (date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    holidaysCount++;
+                    date = date.AddDays(5);
+                }
+
+            }
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
