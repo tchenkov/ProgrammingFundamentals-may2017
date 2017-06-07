813ff4e2b26a5e843d738305ee7b3f611bd2407b
Todor Chenkov <joto_to@mail.bg>
2017-06-07 21:52:19 +0300

    -add P08_MultiplyEvensOdds

---
+++ L09_MethodsDebuggingAndTroubleshootingCode-Lab/P08_MultiplyEvensOdds/P08_MultiplyEvensOdds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P08_MultiplyEvensOdds
+{
+    class P08_MultiplyEvensOdds
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(
+                GetMultipleOfEvensAndOdds(number)
+                );
+        }
+
+        static int GetMultipleOfEvensAndOdds(int number)
+        {
+            number = Math.Abs(number);
+            int evenDigitSum = GetSumOfOddOrEvenDigits(number, true);
+            int oddDigitSum = GetSumOfOddOrEvenDigits(number, false);
+            return evenDigitSum * oddDigitSum;
+        }
+        
+        static int GetSumOfOddOrEvenDigits(int number, bool isEven)
+        {
+            int remainder = isEven ? 0 : 1;
+            int sum = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == remainder)
+                {
+                    sum += digit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
