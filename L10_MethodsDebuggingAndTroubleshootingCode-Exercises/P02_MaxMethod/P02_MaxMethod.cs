a7f1678844044fa9237eaf6b8be5c853da9a31f0
Todor Chenkov <joto_to@mail.bg>
2017-06-08 18:25:03 +0300

    -add P02_MaxMethod

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P02_MaxMethod/P02_MaxMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P02_MaxMethod
+{
+    class P02_MaxMethod
+    {
+        static void Main(string[] args)
+        {
+            var number1 = int.Parse(Console.ReadLine());
+            var number2 = int.Parse(Console.ReadLine());
+            var number3 = int.Parse(Console.ReadLine());
+            var maxNumber = GetMaxNumber(number1, number2, number3);
+            Console.WriteLine(maxNumber);
+        }
+
+        private static int GetMaxNumber(params int[] numbers)
+        {
+            int maxNumber = int.MinValue;
+            foreach (var num in numbers)
+            {
+                maxNumber = num > maxNumber ? num : maxNumber;
+            }
+            return maxNumber;
+        }
+    }
+}
