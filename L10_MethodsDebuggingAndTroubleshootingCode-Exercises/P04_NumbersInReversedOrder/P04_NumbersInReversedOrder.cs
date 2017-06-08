e1f194a8d97b2e00679d85c65586d73720c482dd
Todor Chenkov <joto_to@mail.bg>
2017-06-08 19:31:37 +0300

    -add P04_NumbersInReversedOrder

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P04_NumbersInReversedOrder/P04_NumbersInReversedOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P04_NumbersInReversedOrder
+{
+    class P04_NumbersInReversedOrder
+    {
+        static void Main(string[] args)
+        {
+            var number = decimal.Parse(Console.ReadLine());
+            var reversedNumber = GetReversedNumber(number);
+            Console.WriteLine(reversedNumber);
+        }
+
+        static decimal GetReversedNumber(decimal number)
+        {
+            char[] reverseNumberArray = number.ToString().ToCharArray();
+            Array.Reverse(reverseNumberArray);
+            return decimal.Parse(new string(reverseNumberArray));
+        }
+    }
+}
