5169520fe404a3d284c19f0f14cca21764574005
Todor Chenkov <joto_to@mail.bg>
2017-06-08 18:07:22 +0300

    -add P01_HelloName

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P01_HelloName/P01_HelloName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01_HelloName
+{
+    class P01_HelloName
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            PrintHelloName(name);
+        }
+
+        static void PrintHelloName(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
