3332ff3874f4c5dac2cd7691d0c5d50bf383bd62
Todor Chenkov <joto_to@mail.bg>
2017-06-09 01:53:42 +0300

    -add P11_GeometryCalculator

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P11_GeometryCalculator/P11_GeometryCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace P11_GeometryCalculator
+{
+    class P11_GeometryCalculator
+    {
+        static void Main(string[] args)
+        {
+            var geometryFigure = Console.ReadLine();
+            double result = 0;
+            switch (geometryFigure)
+            {
+                case "triangle":
+                    var sideA = double.Parse(Console.ReadLine());
+                    var heightA = double.Parse(Console.ReadLine());
+                    result = GetTriangleArea(sideA, heightA);
+                    break;
+                case "square":
+                    var squareSide = double.Parse(Console.ReadLine());
+                    result = GetRectangleArea(squareSide, squareSide);
+                    break;
+                case "rectangle":
+                    var rectangleSideA = double.Parse(Console.ReadLine());
+                    var rectangleSideB = double.Parse(Console.ReadLine());
+                    result = GetRectangleArea(rectangleSideA, rectangleSideB);
+                    break;
+                case "circle":
+                    var radius = double.Parse(Console.ReadLine());
+                    result = GetCyrcleArea(radius);
+                    break;
+            }
+            Console.WriteLine($"{result:f2}");
+        }
+
+        private static double GetCyrcleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        private static double GetRectangleArea(double sideA, double sideB)
+        {
+            return sideA * sideB;
+        }
+
+        private static double GetTriangleArea(double sideA, double heightA)
+        {
+            return sideA * heightA / 2;
+        }
+    }
+}
