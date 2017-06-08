70ac0b0a25f202d2842b94ebc15b9fc39291915d
Todor Chenkov <joto_to@mail.bg>
2017-06-08 21:33:31 +0300

    -add P06_PrimeChecker

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P06_PrimeChecker/P06_PrimeChecker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P06_PrimeChecker
+{
+    class P06_PrimeChecker
+    {
+        static void Main(string[] args)
+        {
+
+            var number = long.Parse(Console.ReadLine());
+                       
+            bool isPrime = GetNumPrimeStatus(number);
+            Console.WriteLine(isPrime);
+        }
+
+        static bool GetNumPrimeStatus(long number)
+        {
+            
+            if (number < 2)
+            {
+                return false;
+            }
+            if (number < 4)
+            {
+                return true;
+            }
+            for (int i = 5; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
