8238ccebd2d8e28bdb46ab0287054a940d88ba8d
Todor Chenkov <joto_to@mail.bg>
2017-06-08 19:05:33 +0300

    -add P03_EnglishNameOfLastDigit

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P03_EnglishNameOfLastDigit/P03_EnglishNameOfLastDigit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P03_EnglishNameOfLastDigit
+{
+    class P03_EnglishNameOfLastDigit
+    {
+        static void Main(string[] args)
+        {
+            var number = long.Parse(Console.ReadLine());
+            string lastDigitInEnglish = GetLastDigitInEnglish(number);
+            Console.WriteLine(lastDigitInEnglish);
+        }
+
+        static string GetLastDigitInEnglish(long number)
+        {
+            string numberString = number.ToString();
+            int digit = int.Parse(numberString.Substring(numberString.Length - 1));
+           
+            string[] digitsNames = new string[] 
+                {
+                    "zero",
+                    "one",
+                    "two",
+                    "tree",
+                    "four",
+                    "five",
+                    "six",
+                    "seven",
+                    "eight",
+                    "nine"
+                };
+            return digitsNames[digit];
+        }
+    }
+}
