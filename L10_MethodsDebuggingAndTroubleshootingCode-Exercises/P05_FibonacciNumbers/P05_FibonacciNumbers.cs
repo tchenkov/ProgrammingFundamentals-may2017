271551f94b120dabd344ec3ccebd9c5bbfff927d
Todor Chenkov <joto_to@mail.bg>
2017-06-08 19:41:15 +0300

    -add P05_FibonacciNumbers

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P05_FibonacciNumbers/P05_FibonacciNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P05_FibonacciNumbers
+{
+    class P05_FibonacciNumbers
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int fibMinus2 = 0;
+            int fibMinus1 = 1;
+            int fibonadcciN = 1;
+            for (int i = 0; i < number; i++)
+            {
+                fibonadcciN = fibMinus2 + fibMinus1;
+                fibMinus2 = fibMinus1;
+                fibMinus1 = fibonadcciN;
+            }
+
+            Console.WriteLine(fibonadcciN);
+        }
+    }
+}
