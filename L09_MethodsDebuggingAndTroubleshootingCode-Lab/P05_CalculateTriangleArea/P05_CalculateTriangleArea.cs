9acdc24bd43606913c6db88fc261eb765be30e3d
Todor Chenkov <joto_to@mail.bg>
2017-06-08 16:29:47 +0300

    -update P05_CalculateTriangleArea

--- L09_MethodsDebuggingAndTroubleshootingCode-Lab/P05_CalculateTriangleArea/P05_CalculateTriangleArea.cs
+++ L09_MethodsDebuggingAndTroubleshootingCode-Lab/P05_CalculateTriangleArea/P05_CalculateTriangleArea.cs
@@ -8,7 +8,8 @@ namespace P05_CalculateTriangleArea
         {
             double sideA = double.Parse(Console.ReadLine());
             double HeightA = double.Parse(Console.ReadLine());
-            Console.WriteLine(GetTriangleArea(sideA, HeightA));
+            double triangleArea = GetTriangleArea(sideA, HeightA);
+            Console.WriteLine(triangleArea);
         }
 
         static double GetTriangleArea(double sideA, double heightA)
