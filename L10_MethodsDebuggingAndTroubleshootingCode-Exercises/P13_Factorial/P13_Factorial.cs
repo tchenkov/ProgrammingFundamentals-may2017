1e085b40c9e6887ede8b457f257c00495ce2f40b
Todor Chenkov <joto_to@mail.bg>
2017-06-09 15:39:46 +0300

    -add P13_Factorial

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P13_Factorial/P13_Factorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace P13_Factorial
+{
+    class P13_Factorial
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            PrintFactorialOF(number);
+        }
+
+        static void PrintFactorialOF(int number)
+        {
+            BigInteger factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
