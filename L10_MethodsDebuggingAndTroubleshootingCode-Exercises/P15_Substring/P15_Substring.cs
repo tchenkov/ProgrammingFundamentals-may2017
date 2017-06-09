8a2d3ef180f0f0be732c2bc6ba8745d5fa80cbe9
Todor Chenkov <joto_to@mail.bg>
2017-06-09 16:42:31 +0300

    -add P15_Substring

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P15_Substring/P15_Substring.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P15_Substring
+{
+    class P15_Substring
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            const char magicLetter = 'p';
+            bool hasMatch = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == magicLetter)
+                {
+                    hasMatch = true;
+                    int endIndex = count + 1 > text.Length - i ? text.Length - i : count + 1;
+                    string matchedString = text.Substring(i, endIndex);
+                    Console.WriteLine(matchedString);
+                    i += count;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
