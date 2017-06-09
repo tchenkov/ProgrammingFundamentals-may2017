06d04cfbf79d9d96c64e88809030799593e53aa3
Todor Chenkov <joto_to@mail.bg>
2017-06-09 15:48:26 +0300

    -add P14_FactorialTrailingZeroes

---
+++ L10_MethodsDebuggingAndTroubleshootingCode-Exercises/P14_FactorialTrailingZeroes/P14_FactorialTrailingZeroes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace P14_FactorialTrailingZeroes
+{
+    class P14_FactorialTrailingZeroes
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            PrintFactorialTraillingZeroesCount(number);
+        }
+
+        static void PrintFactorialTraillingZeroesCount(int number)
+        {
+            BigInteger factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+            }
+            
+            int traillingZeroesCount = 0;
+            while (factorial % 10 == 0)
+            {
+                traillingZeroesCount++;
+                factorial /= 10;
+            }
+            Console.WriteLine(traillingZeroesCount);
+        }
+    }
+}
