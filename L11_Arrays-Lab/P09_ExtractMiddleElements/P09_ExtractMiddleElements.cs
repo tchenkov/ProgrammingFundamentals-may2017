0aa3321feeb5376a12dc26a8b3f590248707115b
Todor Chenkov <joto_to@mail.bg>
2017-06-10 21:51:15 +0300

    -add P09_ExtractMiddleElements

---
+++ L11_Arrays-Lab/P09_ExtractMiddleElements/P09_ExtractMiddleElements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace P09_ExtractMiddleElements
+{
+    class P09_ExtractMiddleElements
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfInts = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int middleIndex = arrayOfInts.Length / 2;
+            Console.WriteLine(
+                arrayOfInts.Length == 1 ?
+                $"{arrayOfInts[0]}" :
+                arrayOfInts.Length % 2 == 1 ?
+                $"{arrayOfInts[middleIndex - 1]}\n{arrayOfInts[middleIndex]}\n{arrayOfInts[middleIndex + 1]}" :
+                $"{arrayOfInts[middleIndex - 1]}\n{arrayOfInts[middleIndex]}"
+            );
+        }
+    }
+}
