1465e4161fa9fc6dffb52862668f8ca5870627c3
Todor Chenkov <joto_to@mail.bg>
2017-06-10 17:52:27 +0300

    -add P06_ReverseArrayOfStrings

---
+++ L11_Arrays-Lab/P06_ReverseArrayOfStrings/P06_ReverseArrayOfStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace P06_ReverseArrayOfStrings
+{
+    class P06_ReverseArrayOfStrings
+    {
+        static void Main(string[] args)
+        {
+            string[] inputStrArrReverced = Console.ReadLine()
+                .Split(' ')
+                .ToArray()
+                .Reverse()
+                .ToArray();
+            Console.WriteLine(string.Join(" ", inputStrArrReverced));
+        }
+    }
+}
