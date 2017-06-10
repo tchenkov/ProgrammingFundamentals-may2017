ea5143cc3c891a4cc58a9c394731c353e3bfd3b6
Todor Chenkov <joto_to@mail.bg>
2017-06-10 16:13:22 +0300

    -add P04_TripleSum

---
+++ L11_Arrays-Lab/P04_TripleSum/P04_TripleSum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_TripleSum
+{
+    class P04_TripleSum
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                        .Split(' ')
+                        .Select(int.Parse)
+                        .ToArray();
+
+            bool isThereASum = false;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    // solution #2
+                    int sum = array[i] + array[j];
+                    if (array.Contains(sum))
+                    {
+                        Console.WriteLine($"{array[i]} + {array[j]} == {sum}");
+                        isThereASum = true;
+                    }
+
+                    // solution #1
+                    //for (int k = 0; k < array.Length; k++)
+                    //{
+                    //    if (sum == array[k])
+                    //    {
+                    //        isThereASum = true;
+                    //        Console.WriteLine($"{array[i]} + {array[j]} == {array[k]}");
+                    //        break;
+                    //    }
+                    //}
+                }
+            }
+
+            if (!isThereASum)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
