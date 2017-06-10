739975a73803937f620cbb5f0c1901b37f2b7230
Todor Chenkov <joto_to@mail.bg>
2017-06-10 14:21:04 +0300

    -add P01_DayOfWeek

---
+++ L11_Arrays-Lab/P01_DayOfWeek/P01_DayOfWeek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01_DayOfWeek
+{
+    class P01_DayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            string[] NamesOfDays = new string[] 
+            {
+                "Invalid Day!", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+            };
+            int dayNumber = int.Parse(Console.ReadLine());
+            dayNumber = 0 <= dayNumber && dayNumber <= 7 ? dayNumber : 0;
+            Console.WriteLine(NamesOfDays[dayNumber]);
+        }
+    }
+}
