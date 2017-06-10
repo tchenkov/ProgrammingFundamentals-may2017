3adee1d2d826ed6cd8400912ac86438d0f0d7536
Todor Chenkov <joto_to@mail.bg>
2017-06-10 14:49:27 +0300

    -add P02_ReverseArrayOfIntegers

---
+++ L11_Arrays-Lab/P02_ReverseArrayOfIntegers/P02_ReverseArrayOfIntegers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P02_ReverseArrayOfIntegers
+{
+    class P02_ReverseArrayOfIntegers
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = int.Parse(Console.ReadLine());
+            
+            int[] intArray = new int[arraySize];
+            for (int i = arraySize - 1; i >= 0; i--)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(" ", intArray));
+        }        
+    }
+}
