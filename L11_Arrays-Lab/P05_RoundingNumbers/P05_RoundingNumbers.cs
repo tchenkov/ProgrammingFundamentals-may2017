6ac05952af2bcc66c9f6bb66a6d969d33e62add2
Todor Chenkov <joto_to@mail.bg>
2017-06-10 17:34:32 +0300

    -add P05_RoundingNumbers

---
+++ L11_Arrays-Lab/P05_RoundingNumbers/P05_RoundingNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace P05_RoundingNumbers
+{
+    class P05_RoundingNumbers
+    {
+        static void Main(string[] args)
+        {
+            double[] inputArray = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+            double[] outputArray = (double[]) inputArray.Clone();
+            outputArray = inputArray
+                .Select(element =>  Math.Round(element, MidpointRounding.AwayFromZero))
+                .ToArray();
+            foreach (var arrayElement in inputArray.Zip(outputArray, Tuple.Create))
+            {
+                Console.WriteLine($"{arrayElement.Item1} => {arrayElement.Item2}");
+            }
+        }
+    }
+}
