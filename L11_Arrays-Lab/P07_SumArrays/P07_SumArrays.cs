2b86542eceae7af6f95f83a48bb73c70d9d3cde4
Todor Chenkov <joto_to@mail.bg>
2017-06-10 18:11:29 +0300

    -add P07_SumArrays

---
+++ L11_Arrays-Lab/P07_SumArrays/P07_SumArrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace P07_SumArrays
+{
+    class P07_SumArrays
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] array2 = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] sumOfArrays = new int[Math.Max(array1.Length, array2.Length)];
+            for (int i = 0; i < sumOfArrays.Length; i++)
+            {
+                sumOfArrays[i] = array1[i % array1.Length] + array2[i % array2.Length];
+            }
+            Console.WriteLine(string.Join(" ", sumOfArrays));
+        }
+    }
+}
