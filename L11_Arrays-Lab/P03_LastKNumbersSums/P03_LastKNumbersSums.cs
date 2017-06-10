8549cf7ad814e3893561cd756097b6f350f081e5
Todor Chenkov <joto_to@mail.bg>
2017-06-10 15:54:54 +0300

    -add P03_LastKNumbersSums

---
+++ L11_Arrays-Lab/P03_LastKNumbersSums/P03_LastKNumbersSums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P03_LastKNumbersSums
+{
+    class P03_LastKNumbersSums
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = int.Parse(Console.ReadLine());
+            int previousKElements = int.Parse(Console.ReadLine());
+            int[] array = new int[arraySize];
+            array[0] = 1;
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = 1; j <= previousKElements; j++)
+                {
+                    if (i - j >= 0)
+                    {
+                        array[i] += array[i - j];
+                        continue;
+                    }
+                    break;           
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
