c8fb11fa1f3376bb9cea735c6ca11effcdd6c3bd
Todor Chenkov <joto_to@mail.bg>
2017-06-11 18:44:38 +0300

    -add P02_RotateAndSum

---
+++ L12_Arrays-Exercises/P02_RotateAndSum/P02_RotateAndSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_RotateAndSum
+{
+    class P02_RotateAndSum
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int k = int.Parse(Console.ReadLine());
+            int[] sum = new int[inputArray.Length];
+            for (int j = 0; j < k; j++)
+            {
+                ArrayShiftRight(inputArray);
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    sum[i] += inputArray[i];
+                }
+            }
+            Console.WriteLine(string.Join(" ", sum));
+        }
+
+        static void ArrayShiftRight(int[] array)
+        {
+            int mem = array[array.Length - 1];
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                array[i] = array[i - 1];
+            }
+            array[0] = mem;            
+        }
+    }
+}
