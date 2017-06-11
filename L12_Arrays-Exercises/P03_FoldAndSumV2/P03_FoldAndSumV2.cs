9fe2569bfc6814c83bd24a8fedd1d78f56bf042a
Todor Chenkov <joto_to@mail.bg>
2017-06-12 01:13:14 +0300

    -add P03_FoldAndSumV2

---
+++ L12_Arrays-Exercises/P03_FoldAndSumV2/P03_FoldAndSumV2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_FoldAndSumV2
+{
+    class P03_FoldAndSumV2
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] foldedArray = new int[array.Length / 2];
+
+            int arrayIndex = (foldedArray.Length / 2) - 1;
+            for (int i = 0; i < foldedArray.Length / 2; i++, arrayIndex--)
+            {
+                foldedArray[i] = array[arrayIndex];
+            }
+
+            arrayIndex = array.Length - (array.Length/4);
+            for (int i = foldedArray.Length - 1; i >= foldedArray.Length / 2; i--, arrayIndex++)
+            {
+                foldedArray[i] = array[arrayIndex];
+            }
+
+            for (int i = 0; i < foldedArray.Length; i++)
+            {
+                foldedArray[i] += array[i + (array.Length/ 4)];
+            }
+
+            Console.WriteLine(string.Join(" ", foldedArray));
+        }
+    }
+}
