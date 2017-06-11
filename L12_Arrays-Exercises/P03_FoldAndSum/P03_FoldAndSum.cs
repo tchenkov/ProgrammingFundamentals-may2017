4f6fa0900e2883233a96643385a2c1012870adb3
Todor Chenkov <joto_to@mail.bg>
2017-06-12 00:56:13 +0300

    -add P03_FoldAndSum

---
+++ L12_Arrays-Exercises/P03_FoldAndSum/P03_FoldAndSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_FoldAndSum
+{
+    class P03_FoldAndSum
+    {
+        static void Main(string[] args)
+        {
+            var arrayInput = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            var arrayInputLastQuaterReverced = arrayInput
+                .Reverse()
+                .Take(arrayInput.Length / 4)
+                .ToArray();
+            var arrayInputFolded = arrayInput
+                .Take(arrayInput.Length / 4)
+                .Reverse()
+                .Concat(arrayInputLastQuaterReverced)
+                .ToArray();
+            var arrayInputEdited = arrayInput
+                .Skip(arrayInput.Length / 4)
+                .Take(arrayInput.Length / 2)
+                .ToArray();
+            
+            foreach (var item in arrayInputEdited.Zip(arrayInputFolded, Tuple.Create))
+            {
+                Console.Write($"{item.Item1 + item.Item2} ");
+            }
+        }
+    }
+}
