8cad518f5991fc8100154c250234b3f4290c5832
Todor Chenkov <joto_to@mail.bg>
2017-06-13 22:29:38 +0300

    -add P11_EqualSums

---
+++ L12_Arrays-Exercises/P11_EqualSums/P11_EqualSums.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace P11_EqualSums
+{
+    class P11_EqualSums
+    {
+        static void Main(string[] args)
+        {
+            var numArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            if (numArray.Length == 1)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                leftSum = numArray
+                    .Take(i)
+                    .Sum();
+                rightSum = numArray
+                    .Skip(i + 1)
+                    .Sum();
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
