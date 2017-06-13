7308caba8a59050442ec8a108bb811348899e46d
Todor Chenkov <joto_to@mail.bg>
2017-06-13 20:51:09 +0300

    -add P09_IndexOfLetters

---
+++ L12_Arrays-Exercises/P09_IndexOfLetters/P09_IndexOfLetters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P09_IndexOfLetters
+{
+    class P09_IndexOfLetters
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine().ToLower();
+            var chars = new char['z' - 'a' + 1];
+            for (int i = 'a'; i <= 'z'; i++)
+            {
+                chars[i - 'a'] = (char)i;                
+            }
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine($"{word[i]} -> {Array.IndexOf(chars, word[i])}");
+            }
+        }
+    }
+}
