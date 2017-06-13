97392abfa887c657e256da2657a1a04d19336a4c
Todor Chenkov <joto_to@mail.bg>
2017-06-14 01:53:34 +0300

    -add P05_PizzaIngredients -update exercises conditions

---
+++ L13_ArraysAndMethods-MoreExercises/P05_PizzaIngredients/P05_PizzaIngredients.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_PizzaIngredients
+{
+    class P05_PizzaIngredients
+    {
+        static void Main(string[] args)
+        {
+            var ingredients = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+            var maxIngredientLenght = int.Parse(Console.ReadLine());
+            int ingredientsCount = 0;
+            var addedIngredients = new List<string>();
+            for (int i = 0; i < ingredients.Length && ingredientsCount < 10; i++)
+            {
+                if (ingredients[i].Length == maxIngredientLenght)
+                {
+                    addedIngredients.Add(ingredients[i]);
+                    Console.WriteLine($"Adding {ingredients[i]}.");
+                    ingredientsCount++;
+                }
+            }
+            Console.WriteLine($"Made pizza with total of {ingredientsCount} ingredients.");
+            Console.WriteLine($"The ingredients are: {string.Join(", ", addedIngredients)}.");
+        }
+    }
+}
