0b09709c78f497c75c27b3c8930bd61ee025a45c
Todor Chenkov <joto_to@mail.bg>
2017-06-13 22:58:35 +0300

    -add P01_Array_Statistics

---
+++ L13_ArraysAndMethods-MoreExercises/P01_Array Statistics/P01_Array_Statistics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_Array_Statistics
+{
+    class P01_Array_Statistics
+    {
+        static void Main(string[] args)
+        {
+            var numArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int min = numArray.Min();
+            int max = numArray.Max();
+            int sum = numArray.Sum();
+            double average = numArray.Average();
+
+            Console.WriteLine($"Min = {min}");
+            Console.WriteLine($"Max = {max}");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {average}");
+        }
+    }
+}
