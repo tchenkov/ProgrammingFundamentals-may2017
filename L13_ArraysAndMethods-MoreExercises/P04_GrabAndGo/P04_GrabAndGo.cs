c07cfd36e8cbe4e9813b201dbfd1effa61077624
Todor Chenkov <joto_to@mail.bg>
2017-06-14 00:26:00 +0300

    -add P04_GrabAndGo

---
+++ L13_ArraysAndMethods-MoreExercises/P04_GrabAndGo/P04_GrabAndGo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_GrabAndGo
+{
+    class P04_GrabAndGo
+    {
+        static void Main(string[] args)
+        {
+            var numArray = Console.ReadLine()
+                .Split(' ')
+                .Select(long.Parse)
+                .ToArray();
+            var findLastNum = long.Parse(Console.ReadLine());
+            if (numArray.Contains(findLastNum))
+            {
+
+
+                int index = 0;
+                for (int i = 0; i < numArray.Length; i++)
+                {
+                    if (numArray[i] == findLastNum)
+                    {
+                        index = i;
+                    }
+                }
+                Console.WriteLine(
+                    numArray
+                    .Take(index)
+                    .Sum()
+                    );
+            }
+            else
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+        }
+    }
+}
