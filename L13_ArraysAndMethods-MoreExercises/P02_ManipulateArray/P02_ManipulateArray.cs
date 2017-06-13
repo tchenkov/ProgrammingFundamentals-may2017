2a23a78fa03daf40ba60d5f4d507d26cc6eefdd1
Todor Chenkov <joto_to@mail.bg>
2017-06-13 23:29:20 +0300

    -add P02_ManipulateArray

---
+++ L13_ArraysAndMethods-MoreExercises/P02_ManipulateArray/P02_ManipulateArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_ManipulateArray
+{
+    class P02_ManipulateArray
+    {
+        static void Main(string[] args)
+        {
+            var wordsList = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                List<string> command = Console.ReadLine().Split(' ').ToList();
+                if (command[0] == "Reverse")
+                {
+                    wordsList.Reverse();
+                    continue;
+                }
+                if (command[0] == "Distinct")
+                {
+                    wordsList = wordsList.Distinct().ToList();
+                    continue;
+                }
+                int index = int.Parse(command[1]);
+                var replacingWord = command[2];
+                wordsList[index] = replacingWord;
+            }
+
+            Console.WriteLine(string.Join(", ", wordsList));
+        }
+    }
+}
