9f379bce098f3ef450d6b531ca2c00be45189c17
Todor Chenkov <joto_to@mail.bg>
2017-06-15 02:45:03 +0300

    -add P04_SplitByWordCasing

---
+++ L14_Lists-Lab/P04_SplitByWordCasing/P04_SplitByWordCasing.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_SplitByWordCasing
+{
+    class P04_SplitByWordCasing
+    {
+        static void Main(string[] args)
+        {
+            var separators = new char[] { ',', ';', ':', '.', '!', '(', ')', '"', '\'', '\\', '/', '[', ']', ' ' };
+            var wordsList = Console.ReadLine()
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+               .ToList();
+            var wordsLowerCase = new List<string>();
+            var wordsMixedCase = new List<string>();
+            var wordsUpperCase = new List<string>();
+            for (int i = 0; i < wordsList.Count; i++)
+            {
+                bool upperCase = false;
+                bool lowerCase = false;
+                bool otherChar = false;
+                foreach (var symbol in wordsList[i])
+                {
+                    if ('A' <= symbol && symbol <= 'Z')
+                    {
+                        upperCase = true;
+                    }
+                    else if ('a' <= symbol && symbol <= 'z')
+                    {
+                        lowerCase = true;
+                    }
+                    else if (!('A' <= symbol && symbol <= 'z'))
+                    {
+                        otherChar = true;
+                    }
+                }
+
+                if (lowerCase && !upperCase && !otherChar)
+                {
+                    wordsLowerCase.Add(wordsList[i]);
+                }
+                else if (upperCase && !lowerCase && !otherChar)
+                {
+                    wordsUpperCase.Add(wordsList[i]);
+                }
+                else
+                {
+                    wordsMixedCase.Add(wordsList[i]);
+                }
+            }
+            
+            Console.WriteLine($"Lower-case: {string.Join(", ", wordsLowerCase)}");
+            Console.WriteLine($"Mixed-case: {string.Join(", ", wordsMixedCase)}");
+            Console.WriteLine($"Upper-case: {string.Join(", ", wordsUpperCase)}");
+        }
+    }
+}
