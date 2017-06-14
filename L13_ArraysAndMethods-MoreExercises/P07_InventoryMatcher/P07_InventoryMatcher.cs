2e263c7dbbec3c00a794cd43526549a5b0dc5ff2
Todor Chenkov <joto_to@mail.bg>
2017-06-14 18:31:34 +0300

    -add P07_InventoryMatcher

---
+++ L13_ArraysAndMethods-MoreExercises/P07_InventoryMatcher/P07_InventoryMatcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace P07_InventoryMatcher
+{
+    class P07_InventoryMatcher
+    {
+        static void Main(string[] args)
+        {
+            var goodsList = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+            var quantities = Console.ReadLine()
+                .Split(' ')
+                .Select(long.Parse)
+                .ToList();
+            var prices = Console.ReadLine()
+                .Split(' ')
+                .Select(decimal.Parse)
+                .ToList();
+            var command = Console.ReadLine();
+            while (command != "done")
+            {
+                int indexOfGoods = goodsList.IndexOf(command);
+                Console.WriteLine(
+                    $"{goodsList[indexOfGoods]} costs: {prices[indexOfGoods]}; Available quantity: {quantities[indexOfGoods]}"
+                    );
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
