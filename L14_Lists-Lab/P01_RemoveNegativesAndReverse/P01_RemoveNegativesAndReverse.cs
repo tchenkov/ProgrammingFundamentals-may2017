44323be2eb239697b3825aa08adc40b4994bf2f8
Todor Chenkov <joto_to@mail.bg>
2017-06-14 22:14:38 +0300

    -add P01_RemoveNegativesAndReverse

---
+++ L14_Lists-Lab/P01_RemoveNegativesAndReverse/P01_RemoveNegativesAndReverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_RemoveNegativesAndReverse
+{
+    class P01_RemoveNegativesAndReverse
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .Where(n => n >= 0)
+                .Reverse()
+                .ToList();
+            Console.WriteLine(
+                numList.Count == 0 ?
+                "empty" :
+                string.Join(" ", numList)
+                );
+        }
+    }
+}
