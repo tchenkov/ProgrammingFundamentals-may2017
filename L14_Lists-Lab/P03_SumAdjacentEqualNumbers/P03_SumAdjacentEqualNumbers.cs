e53ae2dfc5e0b9742d711985ce30ef8de356b6db
Todor Chenkov <joto_to@mail.bg>
2017-06-15 01:24:28 +0300

    -add P03_SumAdjacentEqualNumbers

---
+++ L14_Lists-Lab/P03_SumAdjacentEqualNumbers/P03_SumAdjacentEqualNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_SumAdjacentEqualNumbers
+{
+    class P03_SumAdjacentEqualNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+            int i = 0;
+            while (i < numList.Count - 1)
+            {
+                if (numList[i] == numList[i + 1])
+                {
+                    double num = numList[i] + numList[i + 1];
+                    numList.Insert(i, num);
+                    numList.RemoveAt(i + 1);
+                    numList.RemoveAt(i + 1);
+                    i = 0;
+                    continue;
+                }
+                i++;
+            }
+            Console.WriteLine(string.Join(" ", numList));
+        }
+    }
+}
