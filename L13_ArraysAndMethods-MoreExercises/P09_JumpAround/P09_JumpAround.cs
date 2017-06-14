e745adade562fd13196d5e81137609ff1eb771a6
Todor Chenkov <joto_to@mail.bg>
2017-06-14 19:34:26 +0300

    -add P09_JumpAround

---
+++ L13_ArraysAndMethods-MoreExercises/P09_JumpAround/P09_JumpAround.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace P09_JumpAround
+{
+    class P09_JumpAround
+    {
+        static void Main(string[] args)
+        {
+            var numArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int i = 0;
+            int sum = 0;
+            while (i >= 0)
+            {
+                if (i < numArray.Length)
+                {
+                    sum += numArray[i];
+                    i = i + numArray[i] < numArray.Length ?
+                        i + numArray[i] :
+                        i - numArray[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
