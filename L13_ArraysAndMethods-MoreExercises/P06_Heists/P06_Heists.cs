62d0a5c099eed7c699d5b4cd09806655ccee60af
Todor Chenkov <joto_to@mail.bg>
2017-06-14 18:01:59 +0300

    -add P06_Heists

---
+++ L13_ArraysAndMethods-MoreExercises/P06_Heists/P06_Heists.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace P06_Heists
+{
+    class P06_Heists
+    {
+        static void Main(string[] args)
+        {
+            var lootPricese = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+            var jewelsPrice = lootPricese[0];
+            var goldPrice = lootPricese[1];
+            double totalMoney = 0;
+            var commandString = Console.ReadLine();
+            while (commandString != "Jail Time")
+            {
+                var heistElements = 
+                    commandString
+                    .Split(' ')
+                    .ToArray();
+                int jewelsQuantity = heistElements[0].Where(ch => ch == '%').Count();
+                int goldQuantity = heistElements[0].Where(ch => ch == '$').Count();
+                var currentHeistCost = double.Parse(heistElements[1]);
+                totalMoney += 
+                    (jewelsQuantity * jewelsPrice) +
+                    (goldQuantity * goldPrice) -
+                    currentHeistCost;
+                commandString = Console.ReadLine();
+            }
+
+            Console.WriteLine(
+                totalMoney < 0 ?
+                $"Have to find another job. Lost: {Math.Abs(totalMoney)}." :
+                $"Heists will continue. Total earnings: {totalMoney}."
+                );            
+        }
+    }
+}
