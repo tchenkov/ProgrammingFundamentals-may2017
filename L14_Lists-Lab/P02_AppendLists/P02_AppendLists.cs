dd68f8bb66aa48fc924a6e1c0d0491112148d28c
Todor Chenkov <joto_to@mail.bg>
2017-06-14 22:43:03 +0300

    -add P02_AppendLists

---
+++ L14_Lists-Lab/P02_AppendLists/P02_AppendLists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_AppendLists
+{
+    class P02_AppendLists
+    {
+        static void Main(string[] args)
+        {
+            var stringList = Console.ReadLine()
+                .Split('|')
+                .ToList();
+            var finalList = new List<List<int>>();
+            for (int i = 0; i < stringList.Count; i++)
+            {
+                finalList.Add(new List<int>());
+                finalList[i] = stringList[i]
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+            }
+
+            for (int i = finalList.Count - 1; i >= 0; i--)
+            {
+                Console.Write(string.Join(" ", finalList[i]) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
