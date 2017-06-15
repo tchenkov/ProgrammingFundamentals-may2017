e1d067c8ae15c92e11efd4e24ba2f1a4e9983ef5
Todor Chenkov <joto_to@mail.bg>
2017-06-15 19:41:41 +0300

    -add P06_SquareNumbers

---
+++ L14_Lists-Lab/P06_SquareNumbers/P06_SquareNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P06_SquareNumbers
+{
+    class P06_SquareNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .Where(n => Math.Sqrt(n) == (int)Math.Sqrt(n))
+                .OrderByDescending(n => n)
+                .ToList();
+            Console.WriteLine(string.Join(" ", numList));
+        }
+    }
+}
