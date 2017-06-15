faaa500b6094622b27dd7da6f971bd884087ffe1
Todor Chenkov <joto_to@mail.bg>
2017-06-15 19:55:08 +0300

    -add P07_CountNumbers

---
+++ L14_Lists-Lab/P07_CountNumbers/P07_CountNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07_CountNumbers
+{
+    class P07_CountNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            var numDictionary = new Dictionary<int, int>();
+            foreach (var num in numList)
+            {
+                if (!numDictionary.ContainsKey(num))
+                {
+                    numDictionary.Add(num, 0);
+                }
+                numDictionary[num]++;
+            }
+            foreach (var num in numDictionary.OrderBy(n => n.Key))
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}
