43245c007e4dcf88cef5f6dc4b52e560370353fe
Todor Chenkov <joto_to@mail.bg>
2017-06-17 14:51:44 +0300

    -add P03_SearchForANumber

---
+++ L15_Lists-Exercises/P03_SearchForANumber/P03_SearchForANumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_SearchForANumber
+{
+    class P03_SearchForANumber
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            var commands = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            numList = numList
+                .Take(commands[0])
+                .Skip(commands[1])
+                .ToList();
+            Console.WriteLine(
+                numList.Contains(commands[2]) ?
+                "YES!" :
+                "NO!"
+                );
+        }
+    }
+}
