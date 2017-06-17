4435ccb5be61013cad2fef6bdee010600784e6c6
Todor Chenkov <joto_to@mail.bg>
2017-06-17 14:00:51 +0300

    -add P01_MaxSequenceOfEqualElements

---
+++ L15_Lists-Exercises/P01_MaxSequenceOfEqualElements/P01_MaxSequenceOfEqualElements.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_MaxSequenceOfEqualElements
+{
+    class P01_MaxSequenceOfEqualElements
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            var numListLenth = numList.Count;
+            var currentIndex = 0;
+            var currentCount = 1;
+            var maxIndex = 0;
+            var maxCount = 1;
+            for (int i = 1; i < numListLenth; i++)
+            {
+                if (numList[i] == numList[i - 1])
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    currentCount = 1;
+                    currentIndex = i;
+                }
+
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                    maxIndex = i - maxCount + 1;
+                }
+            }
+            numList = numList
+                .Skip(maxIndex)
+                .Take(maxCount)
+                .ToList();
+            Console.WriteLine(string.Join(" ", numList));
+        }
+    }
+}
