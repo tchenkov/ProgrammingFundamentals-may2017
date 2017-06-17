7a68e0c0f6c70b1ce023326d55dce96afcc4754e
Todor Chenkov <joto_to@mail.bg>
2017-06-17 19:30:09 +0300

    -add P06_SumReversedNumbers

---
+++ L15_Lists-Exercises/P06_SumReversedNumbers/P06_SumReversedNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace P06_SumReversedNumbers
+{
+    class P06_SumReversedNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+            for (int i = 0; i < numList.Count; i++)
+            {
+                numList[i] = ReverseString(numList[i]);
+            }
+
+            Console.WriteLine(numList.Select(int.Parse).ToList().Sum());
+
+        }
+
+        static string ReverseString(string s)
+        {
+            var array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
