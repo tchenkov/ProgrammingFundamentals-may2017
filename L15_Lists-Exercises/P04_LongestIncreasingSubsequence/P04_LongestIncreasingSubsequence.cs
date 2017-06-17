382e341dcac6d9569f1cc04fda44aee2079205d4
Todor Chenkov <joto_to@mail.bg>
2017-06-18 02:50:49 +0300

    -add P04_LongestIncreasingSubsequenceV2 -update P04_LongestIncreasingSubsequence

--- L15_Lists-Exercises/P04_LongestIncreasingSubsequence/P04_LongestIncreasingSubsequence.cs
+++ L15_Lists-Exercises/P04_LongestIncreasingSubsequence/P04_LongestIncreasingSubsequence.cs
@@ -22,14 +22,65 @@ namespace P04_LongestIncreasingSubsequence
                 .Select(n => n = -1)
                 .ToArray();
 
-            for (int i = 0; i < numListLength; i++)
+            for (int i = 1; i < numListLength; i++)
             {
-
+                var counter = 0;
+                bool isCounterUsed = false;
                 for (int j = 0; j < i; j++)
                 {
-                    
+                    if (numList[j] < numList[i])
+                    {
+                        var lastValidIndex = previousLongestEndIndex[i] < 0 ?
+                            0 :
+                            previousLongestEndIndex[i];
+                        var lastValid = previousLongestEndIndex[i] < 0 ?
+                            numList[j] - 1 :
+                            numList[lastValidIndex];
+                        var previousValid = lengths[i] > 2 && previousLongestEndIndex[lastValidIndex] >= 0 ?
+                            numList[previousLongestEndIndex[lastValidIndex]] :
+                            numList[j] - 1;
+                        if (j - 1 < 0)
+                        {
+                            lengths[i]++;
+                            counter++;
+                            isCounterUsed = true;
+                            previousLongestEndIndex[i] = j;
+                            lastValid = numList[j];
+                        }
+                        else if (lastValid >= numList[j] && numList[j] > previousValid)
+                        {
+                            previousLongestEndIndex[i] = j;
+                        }
+                        else if (numList[j] > previousValid)
+                        {
+                            lengths[i]++;
+                            counter++;
+                            isCounterUsed = true;
+                            previousLongestEndIndex[i] = j;
+                        }
+                        else if (counter == 0 && isCounterUsed)
+                        {
+                            lengths[i]++;
+                            previousLongestEndIndex[i] = j;
+                            isCounterUsed = false;
+                        }
+                        if (counter > 0 && previousValid > numList[j] && isCounterUsed)
+                        {
+                            counter--;
+                        }
+                    }
                 }
             }
+
+            var maxSubsequenceIndex = Array.IndexOf(lengths, lengths.Max());
+            var maxSubcequence = new List<int>();
+            for (int i = maxSubsequenceIndex; i >= 0;)
+            {
+                maxSubcequence.Add(numList[i]);
+                i = previousLongestEndIndex[i];
+            }
+            maxSubcequence.Reverse();
+            Console.WriteLine(string.Join(" ", maxSubcequence));
         }
     }
 }
---
+++ L15_Lists-Exercises/P04_LongestIncreasingSubsequenceV2/P04_LongestIncreasingSubsequenceV2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04_LongestIncreasingSubsequenceV2
+{
+    class P04_LongestIncreasingSubsequenceV2
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            var numListLength = numList.Count;
+            var lengths = new int[numListLength]
+                .Select(n => n = 1)
+                .ToArray();
+            var previousLongestEndIndex = lengths
+                .Select(n => n = -1)
+                .ToArray();
+            var bestLength = 0;
+            var lastIndex = -1;
+            for (int anchor = 1; anchor < numListLength; anchor++)
+            {
+                var anchorNum = numList[anchor];
+                for (int j = 0; j < anchor; j++)
+                {
+                    var currentNum = numList[j];
+                    if (currentNum < anchorNum && lengths[j] == lengths[anchor])
+                    {
+                        lengths[anchor]++;
+                        previousLongestEndIndex[anchor] = j;
+                    }
+
+                    if (lengths[anchor] > bestLength)
+                    {
+                        bestLength = lengths[anchor];
+                        lastIndex = anchor;
+                    }
+                }
+            }
+
+            var maxSubsequenceIndex = Array.IndexOf(lengths, lengths.Max());
+            var maxSubcequence = new List<int>();
+            for (int i = maxSubsequenceIndex; i >= 0;)
+            {
+                maxSubcequence.Add(numList[i]);
+                i = previousLongestEndIndex[i];
+            }
+            maxSubcequence.Reverse();
+            Console.WriteLine(string.Join(" ", maxSubcequence));
+        }
+    }
+}
