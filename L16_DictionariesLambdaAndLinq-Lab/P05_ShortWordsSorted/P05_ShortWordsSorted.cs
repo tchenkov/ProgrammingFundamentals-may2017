d2acd01185d95b3f6bc97259399956f234e0bcc7
Todor Chenkov <joto_to@mail.bg>
2017-06-18 15:23:49 +0300

    -add P05_ShortWordsSorted

---
+++ L16_DictionariesLambdaAndLinq-Lab/P05_ShortWordsSorted/P05_ShortWordsSorted.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace P05_ShortWordsSorted
+{
+    class P05_ShortWordsSorted
+    {
+        static void Main(string[] args)
+        {
+            var separators = new char[] {'.',',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' '};
+            var wordsList = Console.ReadLine()
+                .ToLower()
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                .Distinct()
+                .Where(w => w.Length < 5)
+                .OrderBy(w => w)
+                .ToList();
+            Console.WriteLine(string.Join(", ", wordsList));
+        }
+    }
+}
