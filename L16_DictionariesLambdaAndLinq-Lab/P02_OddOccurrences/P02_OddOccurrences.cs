ac4e44a4aa589a076a61357425fd7e8d82f73d7d
Todor Chenkov <joto_to@mail.bg>
2017-06-18 14:45:06 +0300

    -add P02_OddOccurrences

---
+++ L16_DictionariesLambdaAndLinq-Lab/P02_OddOccurrences/P02_OddOccurrences.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_OddOccurrences
+{
+    class P02_OddOccurrences
+    {
+        static void Main(string[] args)
+        {
+            var wordsCollection = Console.ReadLine()
+                .ToLower()
+                .Split(' ');
+
+            var wordsDict = new Dictionary<string, int>();
+            foreach (var word in wordsCollection)
+            {
+                if (!wordsDict.ContainsKey(word))
+                {
+                    wordsDict[word] = 0;
+                }
+                wordsDict[word]++;
+            }
+
+            var wordsList = wordsDict
+                .Where(w => w.Value % 2 == 1)
+                .Select(w => w.Key)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", wordsList));                
+        }
+    }
+}
