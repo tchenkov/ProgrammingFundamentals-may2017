453f26422cdb07eb2ec224801937dfecb135bfe7
Todor Chenkov <joto_to@mail.bg>
2017-06-18 14:30:59 +0300

    -add P01_CountRealNumbers

---
+++ L16_DictionariesLambdaAndLinq-Lab/P01_CountRealNumbers/P01_CountRealNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_CountRealNumbers
+{
+    class P01_CountRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse);
+            var numDict = new SortedDictionary<double, int>();
+            foreach (var num in numList)
+            {
+                if (!numDict.ContainsKey(num))
+                {
+                    numDict[num] = 0;
+                }
+                numDict[num]++;
+            }
+
+            foreach (var item in numDict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
