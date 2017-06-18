bf12cfd01ac195d3a1237cd8e619565479fb45dc
Todor Chenkov <joto_to@mail.bg>
2017-06-18 15:06:18 +0300

    -add P04_Largest3Numbers

---
+++ L16_DictionariesLambdaAndLinq-Lab/P04_Largest3Numbers/P04_Largest3Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_Largest3Numbers
+{
+    class P04_Largest3Numbers
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .OrderByDescending(n => n)
+                .Take(3)
+                .ToList();
+            Console.WriteLine(string.Join(" ", numList));
+        }
+    }
+}
