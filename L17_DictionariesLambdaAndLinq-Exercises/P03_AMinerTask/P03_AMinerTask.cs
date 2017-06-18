e00e59ef513a5c21efd895802c568f598d029262
Todor Chenkov <joto_to@mail.bg>
2017-06-18 17:28:03 +0300

    -add P03_AMinerTask

---
+++ L17_DictionariesLambdaAndLinq-Exercises/P03_AMinerTask/P03_AMinerTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03_AMinerTask
+{
+    class P03_AMinerTask
+    {
+        static void Main(string[] args)
+        {
+            var minedResources = new Dictionary<string, int>();
+            var resource = Console.ReadLine();
+
+            while (resource != "stop")
+            {
+                var quantity = int.Parse(Console.ReadLine());
+
+                if (!minedResources.ContainsKey(resource))
+                {
+                    minedResources[resource] = 0;
+                }
+                minedResources[resource] += quantity;
+
+
+                resource = Console.ReadLine();
+            }
+
+            foreach (var item in minedResources)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
