47a95f8dfb695bc0d654422dffbe06fd1cf7ce78
Todor Chenkov <joto_to@mail.bg>
2017-06-18 16:29:44 +0300

    -add P01_Phonebook

---
+++ L17_DictionariesLambdaAndLinq-Exercises/P01_Phonebook/P01_Phonebook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_Phonebook
+{
+    class P01_Phonebook
+    {
+        static void Main(string[] args)
+        {
+            var command = Console.ReadLine();
+            var phoneBook = new Dictionary<string, string>();
+
+            while (command != "END")
+            {
+                var commandList = command
+                    .Split(' ')
+                    .ToList();
+                var instruction = commandList[0];
+                var name = commandList[1];
+
+                if (instruction == "A")
+                {
+                    var phoneNumber = commandList[2];
+                    if (!phoneBook.ContainsKey(name))
+                    {
+                        phoneBook[name] = string.Empty;
+                    }
+                    phoneBook[name] = phoneNumber;
+                }
+
+                if (instruction == "S")
+                {
+                    Console.WriteLine(
+                        phoneBook.ContainsKey(name) ?
+                        $"{name} -> {phoneBook[name]}" :
+                        $"Contact {name} does not exist."
+                        );
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
