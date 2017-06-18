6ee818794752124812d0c280c1354909a3dbd878
Todor Chenkov <joto_to@mail.bg>
2017-06-18 17:49:41 +0300

    -add P04_FixEmails

---
+++ L17_DictionariesLambdaAndLinq-Exercises/P04_FixEmails/P04_FixEmails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04_FixEmails
+{
+    class P04_FixEmails
+    {
+        static void Main(string[] args)
+        {
+            var addressBook = new Dictionary<string, string>();
+            var name = Console.ReadLine();
+
+            while (name != "stop")
+            {
+                var eMailAddress = Console.ReadLine();
+                bool isEMailValid = !eMailAddress.EndsWith(".us") && !eMailAddress.EndsWith(".uk");
+
+                if (isEMailValid)
+                {
+                    if (!addressBook.ContainsKey(name))
+                    {
+                        addressBook[name] = string.Empty;
+                    }
+                    addressBook[name] = eMailAddress;
+                }
+
+
+                name = Console.ReadLine();
+            }
+
+            foreach (var item in addressBook)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
