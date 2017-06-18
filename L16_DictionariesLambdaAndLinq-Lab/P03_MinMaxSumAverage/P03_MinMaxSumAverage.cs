ec8babd08c210d92c2a98a792fcaabf35887cc78
Todor Chenkov <joto_to@mail.bg>
2017-06-18 15:02:56 +0300

    -add P03_MinMaxSumAverage

---
+++ L16_DictionariesLambdaAndLinq-Lab/P03_MinMaxSumAverage/P03_MinMaxSumAverage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_MinMaxSumAverage
+{
+    class P03_MinMaxSumAverage
+    {
+        static void Main(string[] args)
+        {
+            var numCount = int.Parse(Console.ReadLine());
+            var numList = new List<double>();
+            while (numCount > 0)
+            {
+                numList.Add(int.Parse(Console.ReadLine()));
+                numCount--;
+            }
+                        
+            var min = numList.Min();
+            var max = numList.Max();
+            var sum = numList.Sum();
+            var average = numList.Average();
+
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Min = {min}");
+            Console.WriteLine($"Max = {max}");
+            Console.WriteLine($"Average = {average}");
+        }
+    }
+}
