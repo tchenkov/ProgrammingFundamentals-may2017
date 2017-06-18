b88e67194f25dc08d254cfb1c85afbf47fe21629
Todor Chenkov <joto_to@mail.bg>
2017-06-18 15:40:52 +0300

    -add P06_FoldAndSum

---
+++ L16_DictionariesLambdaAndLinq-Lab/P06_FoldAndSum/P06_FoldAndSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06_FoldAndSum
+{
+    class P06_FoldAndSum
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            var quaterLenth = numList.Count / 4;
+            var foldedNums = numList
+                .Skip(quaterLenth * 3)
+                .Reverse()
+                .ToList();
+            foldedNums = new List<int>(
+                numList
+                .Take(quaterLenth)
+                .Reverse()
+                .ToList()
+                .Concat(foldedNums)
+                );
+            numList = numList
+                .Skip(quaterLenth)
+                .Take(quaterLenth * 2)
+                .ToList();
+            var result = new List<int>();
+            for (int i = 0; i < quaterLenth * 2; i++)
+            {
+                result.Add(numList[i] + foldedNums[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));    
+        }
+    }
+}
