f936950bb03748b528f0af6ed034b292123f568c
Todor Chenkov <joto_to@mail.bg>
2017-06-21 21:36:19 +0300

    -add P08_LogsAggregator

---
+++ L17_DictionariesLambdaAndLinq-Exercises/P08_LogsAggregator/P08_LogsAggregator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P08_LogsAggregator
+{
+    class P08_LogsAggregator
+    {
+        static void Main(string[] args)
+        {
+            var inputsCount = int.Parse(Console.ReadLine());
+            var userIpList = new Dictionary<string, List<string>>();
+            var userOverAllDuratiuon = new SortedDictionary<string, double>();
+            GetUserIpData(inputsCount, userIpList, userOverAllDuratiuon);
+
+            foreach (var user in userOverAllDuratiuon)
+            {
+                Console.WriteLine($"{user.Key}: {user.Value} [{string.Join(", ", userIpList[user.Key])}]");
+            }
+        }
+
+        static void GetUserIpData(int inputsCount,
+            Dictionary<string, List<string>> userIpList,
+            SortedDictionary<string, double> userOverAllDuratiuon)
+        {
+            for (int i = 0; i < inputsCount; i++)
+            {
+                var inputList = Console.ReadLine()
+                    .Split(' ')
+                    .ToList();
+                var userName = inputList[1];
+                var userIp = inputList[0];
+                var sessionDuration = double.Parse(inputList[2]);
+
+                if (!userIpList.ContainsKey(userName))
+                {
+                    userIpList[userName] = new List<string>();
+                    userOverAllDuratiuon[userName] = 0;
+                }
+
+                userIpList[userName].Add(userIp);
+                userIpList[userName] = userIpList[userName].Distinct().OrderBy(x => x).ToList();
+                userOverAllDuratiuon[userName] += sessionDuration;
+            }
+        }
+    }
+}
