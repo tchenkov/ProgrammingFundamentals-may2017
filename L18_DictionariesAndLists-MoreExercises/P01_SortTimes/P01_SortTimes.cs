93b91f86ac3ccd1901bac3fcbbdf47f07bb98954
Todor Chenkov <joto_to@mail.bg>
2017-06-22 17:39:24 +0300

    -add P01_SortTimes

---
+++ L18_DictionariesAndLists-MoreExercises/P01_SortTimes/P01_SortTimes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_SortTimes
+{
+    class P01_SortTimes
+    {
+        static void Main(string[] args)
+        {
+            var timeList = Console.ReadLine()
+                .Split(' ')
+                .OrderBy(t => t)
+                .ToList();
+            Console.WriteLine(string.Join(", ", timeList));
+        }
+    }
+}
