bae19703cb84f2eb202b55e5084696402b9569c8
Todor Chenkov <joto_to@mail.bg>
2017-06-22 18:03:38 +0300

    -add P02_OddFilter

---
+++ L18_DictionariesAndLists-MoreExercises/P02_OddFilter/P02_OddFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_OddFilter
+{
+    class P02_OddFilter
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .ToList();
+            var numListAvetage = numList.Average();
+            numList = numList.Select(x => x = x > numListAvetage ? ++x : --x)
+                .ToList();
+            Console.WriteLine(string.Join(" ", numList));
+               
+        }
+    }
+}
