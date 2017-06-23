df8d7ce4337e8b1908c967f88a350637c1778dca
Todor Chenkov <joto_to@mail.bg>
2017-06-23 22:35:15 +0300

    -update P10_SrybskoUnleashed

--- L17_DictionariesLambdaAndLinq-Exercises/P10_SrybskoUnleashed/P10_SrybskoUnleashed.cs
+++ L17_DictionariesLambdaAndLinq-Exercises/P10_SrybskoUnleashed/P10_SrybskoUnleashed.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace P10_SrybskoUnleashed
 {
@@ -10,53 +8,66 @@ namespace P10_SrybskoUnleashed
     {
         static void Main(string[] args)
         {
-            var command = Console.ReadLine();
             var venueSingerProfit = new Dictionary<string, Dictionary<string, long>>();
-            var venueTotalProfit = new Dictionary<string, long>();
 
+            GetVenueSingerProfit(venueSingerProfit);
+
+            PrintVenueSingerProfit(venueSingerProfit);
+        }
+        
+        static void GetVenueSingerProfit(Dictionary<string, Dictionary<string, long>> venueSingerProfit)
+        {
+            var command = Console.ReadLine();
             while (command != "End")
             {
-                var singer = command.Split('@').First().ToString().Trim();
-                var concertInfo = command.Split('@').Last().ToString();
-                var concertInfoList = concertInfo.Split(' ').ToList();
-                if (concertInfoList.Count < 3)
+                var singer = command.Split('@').First();
+                var concertData = command.Substring(command.IndexOf('@') + 1);
+                if (!singer.EndsWith(" ") || singer.Length < 2)
                 {
-                    command = Console.ReadLine();   
+                    command = Console.ReadLine();
                     continue;
                 }
-                var ticketPrice = int.Parse(concertInfoList[concertInfoList.Count - 2]);
-                var ticketsCount = long.Parse(concertInfoList[concertInfoList.Count - 1]);
-                concertInfoList.RemoveRange(concertInfoList.Count - 2, 2);
-                var venue = string.Join(" ", concertInfoList);
-                if (!venueTotalProfit.ContainsKey(venue))
+                singer = singer.Trim();
+
+                var concertDataList = concertData.Split(' ').ToList();
+
+                if (!int.TryParse(concertDataList[concertDataList.Count - 1], out int ticketCount) ||
+                    !int.TryParse(concertDataList[concertDataList.Count - 2], out int ticketPrice) ||
+                    concertDataList.Count < 3)
                 {
-                    venueTotalProfit[venue] = 0;
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                var venue = string.Join(" ",
+                    concertDataList.Take(concertDataList.Count - 2));
+
+                if (!venueSingerProfit.ContainsKey(venue))
+                {
                     venueSingerProfit[venue] = new Dictionary<string, long>();
                 }
                 if (!venueSingerProfit[venue].ContainsKey(singer))
                 {
                     venueSingerProfit[venue][singer] = 0;
                 }
-                var currentProfit = ticketPrice * ticketsCount;
-                venueTotalProfit[venue] += currentProfit;
-                venueSingerProfit[venue][singer] += currentProfit;
+                venueSingerProfit[venue][singer] += ticketPrice * ticketCount;
                 venueSingerProfit[venue] = venueSingerProfit[venue]
                     .OrderByDescending(p => p.Value)
-                    .ThenBy(k => k.Key)
                     .ToDictionary(k => k.Key, p => p.Value);
-
 
                 command = Console.ReadLine();
             }
+        }
 
-            foreach (var venue in venueTotalProfit)
+        static void PrintVenueSingerProfit(Dictionary<string, Dictionary<string, long>> venueSingerProfit)
+        {
+            foreach (var venue in venueSingerProfit)
             {
                 Console.WriteLine(venue.Key);
-
-                foreach (var singer in venueSingerProfit[venue.Key])
-                {
-                    Console.WriteLine($"#  {singer.Key} -> {singer.Value}");
-                }
+                Console.WriteLine(string.Join("\n",
+                    venue.Value
+                    .Select(s => $"#  {s.Key} -> {s.Value}")
+                    ));
             }
         }
     }
