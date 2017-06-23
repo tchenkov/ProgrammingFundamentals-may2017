45a7877fe6a77cb9bb17df888a7867fae343eac5
Todor Chenkov <joto_to@mail.bg>
2017-06-24 00:38:47 +0300

    -add P11_DragonArmy

---
+++ L17_DictionariesLambdaAndLinq-Exercises/P11_DragonArmy/P11_DragonArmy.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P11_DragonArmy
+{
+    class P11_DragonArmy
+    {
+        static void Main(string[] args)
+        {
+            var dragonsTypeNameStats = new Dictionary<string, SortedDictionary<string, List<int>>>();
+
+            GetDragonsInfo(dragonsTypeNameStats);
+
+            PrintDragonInfo(dragonsTypeNameStats);
+        }
+
+        static void GetDragonsInfo(Dictionary<string, SortedDictionary<string, List<int>>> dragonsTypeNameStats)
+        {
+            var dragonsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < dragonsCount; i++)
+            {
+                var dataList = Console.ReadLine().Split(' ').ToList();
+                var dragonType = dataList[0];
+                var name = dataList[1];
+                int damage = int.TryParse(dataList[2], out damage) ? damage : 45;
+                int health = int.TryParse(dataList[3], out health) ? health : 250;
+                int armor = int.TryParse(dataList[4], out armor) ? armor : 10;
+
+                if (!dragonsTypeNameStats.ContainsKey(dragonType))
+                {
+                    dragonsTypeNameStats[dragonType] = new SortedDictionary<string, List<int>>();
+                }
+                if (!dragonsTypeNameStats[dragonType].ContainsKey(name))
+                {
+                    dragonsTypeNameStats[dragonType][name] = new List<int>();
+                }
+                else
+                {
+                    dragonsTypeNameStats[dragonType][name].Clear();
+                }
+                dragonsTypeNameStats[dragonType][name].Add(damage);
+                dragonsTypeNameStats[dragonType][name].Add(health);
+                dragonsTypeNameStats[dragonType][name].Add(armor);
+            }
+        }
+
+        static string GetDragonTypeAverageStats(SortedDictionary<string, List<int>> dragonNames)
+        {
+            var damageList = new List<int>();
+            var healthList = new List<int>();
+            var armorList = new List<int>();
+            foreach (var name in dragonNames)
+            {
+                damageList.Add(name.Value[0]);
+                healthList.Add(name.Value[1]);
+                armorList.Add(name.Value[2]);
+            }
+
+            return $"{damageList.Average():f2}/{healthList.Average():f2}/{armorList.Average():f2}";
+        }
+
+        static void PrintDragonInfo(Dictionary<string, SortedDictionary<string, List<int>>> dragonsTypeNameStats)
+        {
+            foreach (var dragonType in dragonsTypeNameStats)
+            {
+                Console.WriteLine($"{dragonType.Key}::({GetDragonTypeAverageStats(dragonType.Value)})");
+                Console.WriteLine(string.Join("\n",
+                    dragonType.Value
+                    .Select(s => $"-{s.Key} -> damage: {s.Value[0]}, health: {s.Value[1]}, armor: {s.Value[2]}")
+                    ));
+            }
+        }
+    }
+}
---
+++ L17_DictionariesLambdaAndLinq-Exercises/P11_DragonArmy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("P11_DragonArmy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("P11_DragonArmy")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7abd2a7c-a304-477c-a613-08142cc13fc4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
