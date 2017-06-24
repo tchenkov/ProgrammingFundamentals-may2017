e5d7213c0286421d6054d932e398f2099fa0ef6f
Todor Chenkov <joto_to@mail.bg>
2017-06-24 16:30:36 +0300

    -add P06_ByteFlip

---
+++ L18_DictionariesAndLists-MoreExercises/P06_ByteFlip/P06_ByteFlip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace P06_ByteFlip
+{
+    class P06_ByteFlip
+    {
+        static void Main(string[] args)
+        {
+            var messageCode = Console.ReadLine()
+                .Split(' ')
+                .Where(x => x.Length == 2)
+                .Select(x => new string(x.Reverse().ToArray()))
+                .Select(x => Convert.ToChar(Convert.ToInt32(x, 16)))
+                .Reverse()
+                .ToList();
+            Console.WriteLine(string.Join(null ,messageCode));
+        }
+    }
+}
