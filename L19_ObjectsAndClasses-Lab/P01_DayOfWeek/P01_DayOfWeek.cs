8fc82ba8a12c6a756506422e9c9fb5ea368a1e66
Todor Chenkov <joto_to@mail.bg>
2017-06-25 10:47:38 +0300

    -add P01_DayOfWeek

---
+++ L19_ObjectsAndClasses-Lab/P01_DayOfWeek/P01_DayOfWeek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace P01_DayOfWeek
+{
+    class P01_DayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            var dateTimeTemplate = "d-M-yyyy";
+            var inputDate = Console.ReadLine();
+            var date = DateTime.ParseExact(inputDate, dateTimeTemplate, CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
