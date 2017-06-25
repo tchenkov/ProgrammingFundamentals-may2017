11ce08e928ad86a0cb4e7a80cc9a002e6451533d
Todor Chenkov <joto_to@mail.bg>
2017-06-25 11:06:25 +0300

    -add P03_BigFactorial

---
+++ L19_ObjectsAndClasses-Lab/P03_BigFactorial/P03_BigFactorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace P03_BigFactorial
+{
+    class P03_BigFactorial
+    {
+        static void Main(string[] args)
+        {
+            BigInteger factorial = 1;
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 2 ; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
