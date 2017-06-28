0668710edd42e2e1e4784ba53a02028d06cf0d83
Todor Chenkov <joto_to@mail.bg>
2017-06-29 01:13:39 +0300

    -add P01_OrderByAge

---
+++ L21_ObjectsClassesFilesAndExceptions-MoreExercises/P01_OrderByAge/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_OrderByAge
+{
+    class P01_OrderByAge
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>();
+
+            var command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                var personInfo = command.Split(' ');
+                var personName = personInfo[0];
+                var personId = personInfo[1];
+                var personAge = int.Parse(personInfo[2]);
+
+                var newPerson = new Person
+                {
+                    Name = personName,
+                    IdTag = personId,
+                    Age = personAge
+                };
+                people.Add(newPerson);
+
+                command = Console.ReadLine();
+            }
+
+            people = people.OrderBy(p => p.Age).ToList();
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Name} with ID: {person.IdTag} is {person.Age} years old.");
+            }
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string IdTag { get; set; }
+        public int Age { get; set; }
+    }
+}
