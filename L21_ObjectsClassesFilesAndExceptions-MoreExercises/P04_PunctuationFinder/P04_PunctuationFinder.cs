9ee000fd76e4da829f67a2158690e88340be60a6
Todor Chenkov <joto_to@mail.bg>
2017-06-30 00:49:57 +0300

    -add P04_PunctuationFinder

---
+++ L21_ObjectsClassesFilesAndExceptions-MoreExercises/P04_PunctuationFinder/P04_PunctuationFinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace P04_PunctuationFinder
+{
+    class P04_PunctuationFinder
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText(@"C:\Users\todor\Desktop\sample_text.txt");
+            var result = text.ToCharArray().Where(c => new char[] { '.', ',', '!', '?', ':' }.Contains(c)).ToList();
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
