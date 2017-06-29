e819ae9acfdcbf205de32b9d2c5dd46c22fafd82
Todor Chenkov <joto_to@mail.bg>
2017-06-30 00:56:15 +0300

    -add P05_WriteToFile

---
+++ L21_ObjectsClassesFilesAndExceptions-MoreExercises/P05_WriteToFile/P05_WriteToFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace P05_WriteToFile
+{
+    class P05_WriteToFile
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText(@"C:\Users\todor\Desktop\sample_text.txt");
+            var result = text.ToCharArray().Where(c => !(new char[] { '.', ',', '!', '?', ':' }.Contains(c))).ToList();
+            Console.WriteLine(string.Join("", result));
+        }
+    }
+}
