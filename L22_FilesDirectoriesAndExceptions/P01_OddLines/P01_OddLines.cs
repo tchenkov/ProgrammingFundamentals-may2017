cc8657902a942a061f3b62d1684af3466a3ed95c
Todor Chenkov <joto_to@mail.bg>
2017-06-30 21:52:57 +0300

    -add P01_OddLines

---
+++ L22_FilesDirectoriesAndExceptions/P01_OddLines/P01_OddLines.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace P01_OddLines
+{
+    class P01_OddLines
+    {
+        static void Main(string[] args)
+        {
+            var textLines = File.ReadAllLines(@"C:\Users\todor\Desktop\input.txt");
+            var oddLines = textLines.Where((t, i) => i % 2 == 1).ToArray();
+            File.AppendAllLines(@"C:\Users\todor\Desktop\output.txt", oddLines);
+        }
+    }
+}
