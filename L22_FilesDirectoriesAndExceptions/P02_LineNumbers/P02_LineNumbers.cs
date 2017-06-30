515d5ffb69b63197c662e70c43ab120a832ed36b
Todor Chenkov <joto_to@mail.bg>
2017-06-30 21:57:29 +0300

    -add P02_LineNumbers

---
+++ L22_FilesDirectoriesAndExceptions/P02_LineNumbers/P02_LineNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace P02_LineNumbers
+{
+    class P02_LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            var textLines = File.ReadAllLines(@"C:\Users\todor\Desktop\input.txt");
+            var oddLines = textLines.Select((t, i) => $"{i + 1}. " + t).ToArray();
+            File.AppendAllLines(@"C:\Users\todor\Desktop\output.txt", oddLines);
+        }
+    }
+}
