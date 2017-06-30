0bbc9605d3e1fad3afe0a9bd33b648b92849df15
Todor Chenkov <joto_to@mail.bg>
2017-06-30 22:38:46 +0300

    -add P03_WordCount

---
+++ L22_FilesDirectoriesAndExceptions/P03_WordCount/P03_WordCount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_WordCount
+{
+    class P03_WordCount
+    {
+        static void Main(string[] args)
+        {
+            var words = File.ReadAllText(@"C:\Users\todor\Desktop\words.txt").Split();
+            var text = File.ReadAllText(@"C:\Users\todor\Desktop\input.txt")
+                .Split(new char[] { '\n', '\r', ' ', '.', ',', '!', '?', '-' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            var shortestWordLenght = words.OrderBy(w => w.Length).First().Length;
+            var textLenght = text.Length - shortestWordLenght;
+
+            var selectedWordsCount = new Dictionary<string, int>();
+
+            foreach (var word in text)
+            {
+                foreach (var match in words)
+                {
+                    if (!selectedWordsCount.ContainsKey(match))
+                    {
+                        selectedWordsCount[match] = 0;
+                    }
+                    if (string.Equals(word, match, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        selectedWordsCount[match]++;
+                    }
+                }
+            }
+
+            selectedWordsCount = selectedWordsCount
+                .OrderByDescending(c => c.Value)
+                .ToDictionary(k => k.Key, v => v.Value);
+            var outputtext = selectedWordsCount.Select(word => $"{word.Key} - {word.Value}").ToList();
+            File.WriteAllLines(@"C:\Users\todor\Desktop\output.txt", outputtext);
+        }
+    }
+}
