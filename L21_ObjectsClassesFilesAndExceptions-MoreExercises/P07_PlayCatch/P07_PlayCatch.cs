6982369a8d4f9d1f8d56d2f089bf2720b4e1d284
Todor Chenkov <joto_to@mail.bg>
2017-06-30 20:21:56 +0300

    -add P07_PlayCatch

---
+++ L21_ObjectsClassesFilesAndExceptions-MoreExercises/P07_PlayCatch/P07_PlayCatch.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07_PlayCatch
+{
+    class P07_PlayCatch
+    {
+        static void Main(string[] args)
+        {
+            Catch3Exeptions();
+        }
+
+        static void Catch3Exeptions()
+        {
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var exeptionsLeft = 3;
+            while (exeptionsLeft > 0)
+            {
+                var command = Console.ReadLine();
+                var instuction = command.Split(' ').First();
+                var commandList = command.Split(' ').Skip(1).ToArray();
+                
+                try
+                {
+                    switch (instuction)
+                    {
+                        case "Replace":
+                            RaplaceNum(nums, commandList);
+                            break;
+                        case "Print":
+                            PrintNumRange(nums, commandList);
+                            break;
+                        case "Show":
+                            PrintNumAt(nums, commandList);
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The variable is not in the correct format!");
+                    exeptionsLeft--;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("The index does not exist!");
+                    exeptionsLeft--;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+
+        static void PrintNumAt(int[] nums, string[] commandList)
+        {
+            var index = int.Parse(commandList[0]);
+            Console.WriteLine(nums[index]);
+        }
+
+        static void PrintNumRange(int[] nums, string[] commandList)
+        {
+            var startIndex = int.Parse(commandList[0]);
+            var end = int.Parse(commandList[1]);
+            var result = new List<int>();
+            for (int i = startIndex; i <= end; i++)
+            {
+                result.Add(nums[i]);
+            }
+            Console.WriteLine(string.Join(", ", result));
+        }
+
+        static void RaplaceNum(int[] nums, string[] commandList)
+        {
+            var index = int.Parse(commandList[0]);
+            nums[index] = int.Parse(commandList[1]);
+        }
+    }
+}
---
+++ L21_ObjectsClassesFilesAndExceptions-MoreExercises/P07_PlayCatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("P07_PlayCatch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("P07_PlayCatch")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("208acf88-45eb-496e-b35f-6fc4d478833f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
