1f3d48ca19c557f7e26331c7249920125bc7ce5a
Todor Chenkov <joto_to@mail.bg>
2017-06-30 20:32:33 +0300

    -add P08_PersonalException

---
+++ L21_ObjectsClassesFilesAndExceptions-MoreExercises/P08_PersonalException/P08_PersonalException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P08_PersonalException
+{
+    class P08_PersonalException
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    var num = int.Parse(Console.ReadLine());
+
+                    if (num < 0)
+                    {
+                        throw new NegativeNumberException();
+                    }
+                    Console.WriteLine(num);
+                }
+            }
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public class NegativeNumberException : Exception
+        {
+            public NegativeNumberException() : base("My first exception is awesome!!!")
+            {
+
+            }            
+        }
+    }
+}
