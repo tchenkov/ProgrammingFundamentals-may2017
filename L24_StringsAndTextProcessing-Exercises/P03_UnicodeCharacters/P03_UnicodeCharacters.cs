bfb0212b8b8440a5dbdd3e11566e09e3b959d603
Todor Chenkov <joto_to@mail.bg>
2017-07-01 20:29:20 +0300

    -add P03_UnicodeCharacters

---
+++ L24_StringsAndTextProcessing-Exercises/P03_UnicodeCharacters/P03_UnicodeCharacters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P03_UnicodeCharacters
+{
+    class P03_UnicodeCharacters
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            var uniCode = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var code = $"{(int)text[i]:X4}".ToLower();
+                uniCode += $"\\u{code}";
+            }
+
+            Console.WriteLine(uniCode);
+        }
+    }
+}
