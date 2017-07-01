97d8b2d2af193e16f3b00d67119fbf5467ab0ecb
Todor Chenkov <joto_to@mail.bg>
2017-07-01 18:33:21 +0300

    -add P04_Palindromes

---
+++ L23_StringsAndTextProcessing-Lab/P04_Palindromes/P04_Palindromes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Palindromes
+{
+    class P04_Palindromes
+    {
+        static void Main(string[] args)
+        {
+            var wordsArray = Console.ReadLine()
+                .Split(new char[] { ' ', ',', '.', '?', '!' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            var palindromesList = new List<string>();
+
+            foreach (var word in wordsArray)
+            {
+                var reversedWord = new string(word.Reverse().ToArray());
+                if (word == reversedWord && !palindromesList.Contains(word))
+                {
+                    palindromesList.Add(word);
+                }
+            }
+
+            palindromesList = palindromesList.OrderBy(w => w).ToList();
+            Console.WriteLine(string.Join(", ", palindromesList));
+        }
+    }
+}
