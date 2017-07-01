9e297e25e03322554d377dd7f5ce1c4c3f593ac9
Todor Chenkov <joto_to@mail.bg>
2017-07-01 12:37:42 +0300

    -add P05_FolderSize

---
+++ L22_FilesDirectoriesAndExceptions/P05_FolderSize/P05_FolderSize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace P05_FolderSize
+{
+    class P05_FolderSize
+    {
+        static void Main(string[] args)
+        {
+            var userDesctopPath = 
+                Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Desktop\");
+            var filesInPath = Directory.EnumerateFiles(userDesctopPath).ToList();
+            decimal size = 0m;
+            foreach (var file in filesInPath)
+            {
+                var fileLenth = new FileInfo(file);
+                size += fileLenth.Length;
+            }
+
+            size = size / 1024 / 1024 / 1024;
+            var  sizeString = string.Format("{0} GB",Math.Floor(size*100) / 100);
+
+            File.WriteAllText(userDesctopPath + "dir size.txt", sizeString);
+        }
+    }
+}
