1139415d130cc183a93518d71fbef1a7b695e407
Todor Chenkov <joto_to@mail.bg>
2017-07-01 19:21:37 +0300

    -add P01_ConvertFromBase_10ToBase_N

---
+++ L24_StringsAndTextProcessing-Exercises/P01_ConvertFromBase-10ToBase-N/P01_ConvertFromBase_10ToBase_N.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace P01_ConvertFromBase_10ToBase_N
+{
+    class P01_ConvertFromBase_10ToBase_N
+    {
+        static void Main(string[] args)
+        {
+            var inputLine = Console.ReadLine().Split();
+            var numBase = int.Parse(inputLine[0]);
+            var numberBase10 = BigInteger.Parse(inputLine[1]);
+            var numString = string.Empty;
+
+            while (numberBase10 > 0)
+            {
+                var remainder = numberBase10 % numBase;
+                numString = remainder + numString;
+                numberBase10 /= numBase;
+            }
+
+            Console.WriteLine(numString);
+        }
+    }
+}
