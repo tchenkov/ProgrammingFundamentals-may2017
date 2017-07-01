650ff1ef3d7e40b11852df56f32798ceb150c0e0
Todor Chenkov <joto_to@mail.bg>
2017-07-01 17:45:49 +0300

    -add P02_CountSubstringOccurrences

---
+++ L23_StringsAndTextProcessing-Lab/P02_CountSubstringOccurrences/P02_CountSubstringOccurrences.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P02_CountSubstringOccurrences
+{
+    class P02_CountSubstringOccurrences
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToLower();
+            var substring = Console.ReadLine().ToLower();
+
+            var count = 0;
+            var index = 0;
+          
+            while (index != -1)
+            {
+                index = text.IndexOf(substring, index);
+                if (index >= 0)
+                {
+                    count++;
+                    index++;
+                }                
+            }
+
+        Console.WriteLine(count);
+        }
+    }
+}
