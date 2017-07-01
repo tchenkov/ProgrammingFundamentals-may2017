ab3062147abb123a1dc2013eb340f378afbcc3c6
Todor Chenkov <joto_to@mail.bg>
2017-07-01 11:57:02 +0300

    -add P04_MergeFiles

---
+++ L22_FilesDirectoriesAndExceptions/P04_MergeFiles/P04_MergeFiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace P04_MergeFiles
+{
+    class P04_MergeFiles
+    {
+        static void Main(string[] args)
+        {
+            var path = Environment.ExpandEnvironmentVariables("%USERPROFILE%\\Desktop\\");
+            var text1Lines = File.ReadAllLines(path + "input.txt");
+            var text2Lines = File.ReadAllLines(path + "modified input.txt");
+            var append2TextLines = text1Lines.Concat(text2Lines);
+            File.WriteAllLines(path + "output.txt", append2TextLines);
+        }
+    }
+}
