88d740cfcf94e9ccec4da2d4edf3540d1c59c73e
Todor Chenkov <joto_to@mail.bg>
2017-07-01 16:38:25 +0300

    -add P01_ReverseString

---
+++ L23_StringsAndTextProcessing-Lab/P01_ReverseString/P01_ReverseString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_ReverseString
+{
+    class P01_ReverseString
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            var strReversed = new string(str.Reverse().ToArray());
+            Console.WriteLine(strReversed);
+        }
+    }
+}
