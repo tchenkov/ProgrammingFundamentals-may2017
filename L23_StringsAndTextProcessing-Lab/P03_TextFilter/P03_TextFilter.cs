7ec86a9cba004ce986475dbea3716e125a26922c
Todor Chenkov <joto_to@mail.bg>
2017-07-01 18:14:06 +0300

    -add P03_TextFilter

---
+++ L23_StringsAndTextProcessing-Lab/P03_TextFilter/P03_TextFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P03_TextFilter
+{
+    class P03_TextFilter
+    {
+        static void Main(string[] args)
+        {
+            var bannedWords = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None);
+            var text = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                while (text.Contains(word))
+                {
+                    text = text.Replace(word, new string('*', word.Length));                    
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
