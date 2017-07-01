06f7c391bebaa6a0929d2f7c619280596177673f
Todor Chenkov <joto_to@mail.bg>
2017-07-01 20:58:01 +0300

    -update P04_CharacterMultiplier

--- L24_StringsAndTextProcessing-Exercises/P04_CharacterMultiplier/P04_CharacterMultiplier.cs
+++ L24_StringsAndTextProcessing-Exercises/P04_CharacterMultiplier/P04_CharacterMultiplier.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace P04_CharacterMultiplier
 {
@@ -10,7 +7,24 @@ namespace P04_CharacterMultiplier
     {
         static void Main(string[] args)
         {
+            var input = Console.ReadLine().Split().OrderBy(s => s.Length).ToArray();
+            var str1 = input[0];
+            var str2 = input[1];
 
+            var result = 0;
+            for (int i = 0; i < str2.Length; i++)
+            {
+                if (i < str1.Length)
+                {
+                    result += str1[i] * str2[i];
+                }
+                else
+                {
+                    result += str2[i];
+                }
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
