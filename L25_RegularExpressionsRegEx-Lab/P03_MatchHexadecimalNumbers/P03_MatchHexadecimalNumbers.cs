f3404a34819e3495542e9c9952415df65d472030
Todor Chenkov <joto_to@mail.bg>
2017-07-03 12:37:33 +0300

    -add P03_MatchHexadecimalNumbers

---
+++ L25_RegularExpressionsRegEx-Lab/P03_MatchHexadecimalNumbers/P03_MatchHexadecimalNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P03_MatchHexadecimalNumbers
+{
+    class P03_MatchHexadecimalNumbers
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"\b(0x)?[0-9A-F]+\b";
+            var text = Console.ReadLine();
+            var matches = Regex.Matches(text, pattern);
+            var hexNums = matches
+                .Cast<Match>()
+                .Select(hex => hex.Value)
+                .ToArray();
+            Console.WriteLine(string.Join(" ", hexNums));
+        }
+    }
+}
