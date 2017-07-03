50d5c26a03fe0400b5fa95fb44d855cfcb1f0531
Todor Chenkov <joto_to@mail.bg>
2017-07-03 18:26:39 +0300

    -add P01_ExtractEmails

---
+++ L26_RegularExpressions(RegEx)-Exercises/P01_ExtractEmails/P01_ExtractEmails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace P01_ExtractEmails
+{
+    class P01_ExtractEmails
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            //var path = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Desktop\");
+            //var text = File.ReadAllText(path + "test.txt");
+            var pattern = @"(^|(?<=\s))[A-Za-z]+([0-9]+)?([\._-][A-z0-9]+)?@\w+-?\w+(?:\.\w+)+";
+            var eMailMatches = Regex.Matches(text, pattern);
+            var eMailArr = eMailMatches.Cast<Match>()
+                .Select(e => e.Value)
+                .ToArray();
+            Console.WriteLine(string.Join("\r\n", eMailArr));
+        }
+    }
+}
