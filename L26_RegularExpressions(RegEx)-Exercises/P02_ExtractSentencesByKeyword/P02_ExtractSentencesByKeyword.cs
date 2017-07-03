e50ef925ee37576bb4c1afeed327e3ea376e026b
Todor Chenkov <joto_to@mail.bg>
2017-07-03 19:18:56 +0300

    -add P02_ExtractSentencesByKeyword

---
+++ L26_RegularExpressions(RegEx)-Exercises/P02_ExtractSentencesByKeyword/P02_ExtractSentencesByKeyword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P02_ExtractSentencesByKeyword
+{
+    class P02_ExtractSentencesByKeyword
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+            var text = Console.ReadLine();
+            var sentencePattern = @"(^|(?<=\s))[A-Z][^.!?]*\W" + word + @"\W.*?(?=[\.\?\!])";
+            var sentencesMatches = Regex.Matches(text, sentencePattern);
+            foreach (Match sentence in sentencesMatches)
+            {
+                Console.WriteLine(sentence.Value);
+            }
+        }
+    }
+}
