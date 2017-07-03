703956258171af106a250c73037074ef3ff6df7a
Todor Chenkov <joto_to@mail.bg>
2017-07-03 16:18:57 +0300

    -add P05_MatchNumbers

---
+++ L25_RegularExpressionsRegEx-Lab/P05_MatchNumbers/P05_MatchNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P05_MatchNumbers
+{
+    class P05_MatchNumbers
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            var pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+            var matches = Regex.Matches(text, pattern);
+            var numArr = matches
+                .Cast<Match>()
+                .Select(n => n.Value)
+                .ToArray();
+            Console.WriteLine(string.Join(" ", numArr));
+
+        }
+    }
+}
