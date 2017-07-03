e256e495c7682ba11310fbca78e38f96ff09d85e
Todor Chenkov <joto_to@mail.bg>
2017-07-03 11:30:45 +0300

    -add P01_MatchFullName

---
+++ L25_RegularExpressionsRegEx-Lab/P01_MatchFullName/P01_MatchFullName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace P01_MatchFullName
+{
+    class P01_MatchFullName
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine();
+            var matches = Regex.Matches(names, @"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+            var namesList = new List<string>();
+            foreach (Match name in matches)
+            {
+                namesList.Add($"{name}");
+            }
+            Console.WriteLine(string.Join(" ", namesList));
+        }
+    }
+}
