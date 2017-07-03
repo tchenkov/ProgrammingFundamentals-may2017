b7b3c560dfdaa05da618d6fdd38776bc77e90c9e
Todor Chenkov <joto_to@mail.bg>
2017-07-03 17:02:30 +0300

    -add P06_ReplaceATag

---
+++ L25_RegularExpressionsRegEx-Lab/P06_ReplaceATag/P06_ReplaceATag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace P06_ReplaceATag
+{
+    class P06_ReplaceATag
+    {
+        static void Main(string[] args)
+        {
+            var text = new List<string>();
+            var addText = Console.ReadLine();
+            while (addText != "end")
+            {
+                text.Add(addText);
+                addText = Console.ReadLine();
+            }
+
+            foreach (var line in text)
+            {
+                var pattern = @"<a.*?href=""(?<url>.*?)"".*?>(?<text>.*?)<\/a>";
+                var replacePattern = @"[URL href=""${url}""]${text}[/URL]";
+                var replacedText = Regex.Replace(line, pattern, replacePattern);
+                Console.WriteLine(replacedText);
+            }
+        }
+    }
+}
