b0fd2e2068035556ba702175568587abc3bcad5d
Todor Chenkov <joto_to@mail.bg>
2017-07-05 00:34:44 +0300

    -add P01_Censorship

---
+++ L27_StringsAndRegularExpressions-MoreExercises/P01_Censorship/P01_Censorship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P01_Censorship
+{
+    class P01_Censorship
+    {
+        static void Main(string[] args)
+        {
+            var keyWord = Console.ReadLine();
+            var text = Console.ReadLine();
+            text = Regex.Replace(text, keyWord, new string('*', keyWord.Length));
+            Console.WriteLine(text);
+        }
+    }
+}
