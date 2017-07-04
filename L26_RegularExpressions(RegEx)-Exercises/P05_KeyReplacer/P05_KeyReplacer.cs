b9a3cf7ee291f870c80bd7fc55ce00f11162eec8
Todor Chenkov <joto_to@mail.bg>
2017-07-04 14:41:36 +0300

    -add P05_KeyReplacer

---
+++ L26_RegularExpressions(RegEx)-Exercises/P05_KeyReplacer/P05_KeyReplacer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P05_KeyReplacer
+{
+    class P05_KeyReplacer
+    {
+        static void Main(string[] args)
+        {
+            var keyString = Console.ReadLine();
+
+            var startPattern = @"^[A-Za-z]+(?=[<\|\\])";
+            var endPattern = @"(?<=[<\|\\])[A-Za-z]+(?=$)";
+            var start = Regex.Match(keyString, startPattern).Value;
+            var end = Regex.Match(keyString, endPattern).Value;
+
+            var text = Console.ReadLine();
+
+            var pattern = $"(?<={start})(?!{start}|{end}).*?(?={end})";
+            var matches = Regex.Matches(text, pattern);
+
+            var matchesList = matches
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .ToList();
+            if (matchesList.Count == 0)
+            {
+                Console.WriteLine("Empty result");
+            }
+            else
+            {
+                Console.WriteLine(string.Join("", matchesList));
+            }
+        }
+    }
+}
