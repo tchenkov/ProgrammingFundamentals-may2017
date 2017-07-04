948db50417b69abf023e2b6ee5a8ed6b0c5a8f64
Todor Chenkov <joto_to@mail.bg>
2017-07-04 17:44:44 +0300

    -add P06_ValidUsernames

---
+++ L26_RegularExpressions(RegEx)-Exercises/P06_ValidUsernames/P06_ValidUsernames.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P06_ValidUsernames
+{
+    class P06_ValidUsernames
+    {
+        static void Main(string[] args)
+        {
+            var rawUsernameText = Console.ReadLine();
+            var validUsernamePattern = 
+                @"(?<=^|[ \/\\\(\)])[A-Za-z][A-Za-z\d_]{2,24}(?=$|[ \/\\\(\)])";
+            var usernamesArr =
+                Regex.Matches(rawUsernameText, validUsernamePattern)
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .ToArray();
+            var maxSum = 0;
+            var index = -1;
+            for (int i = 0; i < usernamesArr.Length - 1; i++)
+            {
+                var currentSum = 
+                    usernamesArr[i].Length + usernamesArr[i + 1].Length;
+
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                    index = i;
+                }
+            }
+
+            Console.WriteLine(usernamesArr[index]);
+            Console.WriteLine(usernamesArr[index + 1]);
+        }
+    }
+}
