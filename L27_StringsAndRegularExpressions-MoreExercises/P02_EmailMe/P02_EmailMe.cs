f7aff950078d623ae017039e15ce4783cd0ed2ea
Todor Chenkov <joto_to@mail.bg>
2017-07-05 09:11:43 +0300

    -add P02_EmailMe

---
+++ L27_StringsAndRegularExpressions-MoreExercises/P02_EmailMe/P02_EmailMe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_EmailMe
+{
+    class P02_EmailMe
+    {
+        static void Main(string[] args)
+        {
+            var eMail = Console.ReadLine();
+            var usernameSum = eMail.Split('@')
+                .First()
+                .ToCharArray()
+                .Select(ch => (int) ch)
+                .Sum();
+            var domainSum = eMail
+                .Split('@')
+                .Last()
+                .ToCharArray()
+                .Select(ch => (int)ch)
+                .Sum();
+            Console.WriteLine(
+                usernameSum - domainSum < 0 ?
+                "She is not the one." :
+                "Call her!"
+            );
+        }
+    }
+}
