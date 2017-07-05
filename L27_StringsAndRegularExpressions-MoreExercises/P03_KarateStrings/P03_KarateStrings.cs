e81bee2ab9b9f0131edb7e49ed7f8bc09177419a
Todor Chenkov <joto_to@mail.bg>
2017-07-05 09:43:05 +0300

    -add P03_KarateStrings

---
+++ L27_StringsAndRegularExpressions-MoreExercises/P03_KarateStrings/P03_KarateStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P03_KarateStrings
+{
+    class P03_KarateStrings
+    {
+        static void Main(string[] args)
+        {
+            var inputString = Console.ReadLine();
+            var outputString = string.Empty;
+            var punchStrength = 0;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == '>')
+                {
+                    punchStrength += int.Parse( inputString[i + 1].ToString());
+                }
+                else if (punchStrength > 0)
+                {
+                    punchStrength--;
+                    continue;
+                }
+                outputString += inputString[i];
+            }
+
+            Console.WriteLine(outputString);
+        }
+    }
+}
