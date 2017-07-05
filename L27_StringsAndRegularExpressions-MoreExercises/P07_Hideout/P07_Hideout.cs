d94387ea26766e6a006f3a7043604514ad334588
Todor Chenkov <joto_to@mail.bg>
2017-07-05 16:21:01 +0300

    -add P07_Hideout

---
+++ L27_StringsAndRegularExpressions-MoreExercises/P07_Hideout/P07_Hideout.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P07_Hideout
+{
+    class P07_Hideout
+    {
+        static void Main(string[] args)
+        {
+            var map = Console.ReadLine();
+            var isClueFound = false;
+            while (!isClueFound)
+            {
+                var specialChars = @"/.*+?|(,)[]{}\";
+                var clue = Console.ReadLine().Split();
+                var clueChar = clue.First();
+                clueChar = specialChars.Contains(clueChar) ?
+                    @"\"+clueChar :
+                    clueChar;
+                var minCount = int.Parse(clue.Last());
+                var match = Regex.Match(map, $"[{clueChar}]{{{minCount},}}");
+                isClueFound = match.Success;
+                if (isClueFound)
+                {
+                    var index = match.Index;
+                    var length = match.Value.Length;
+
+                    Console.WriteLine($"Hideout found at index {index} and it is with size {length}!");
+                }
+            }
+        }
+    }
+}
