26c43bb0be1ec4d6110350d015b50d8fe54a53fe
Todor Chenkov <joto_to@mail.bg>
2017-07-05 18:45:15 +0300

    -add E01_SinoTheWalker v1 and v2

---
+++ L28_Exam Preparation I/E01_SinoTheWalker/E01_SinoTheWalker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace E01_SinoTheWalker
+{
+    class E01_SinoTheWalker
+    {
+        static void Main(string[] args)
+        {   // the very bad but working version
+            var startTime = Console.ReadLine()
+                .Split(':')
+                .Select(int.Parse)
+                .ToArray();
+            var stepsCount = long.Parse(Console.ReadLine());
+            var secondsPerStep = int.Parse(Console.ReadLine());
+            var travelTime = stepsCount * secondsPerStep;
+            travelTime = travelTime % (3600 * 24);
+            var travelSeconds = travelTime % 60;
+            travelTime = (travelTime - travelSeconds) / 60;
+            var travelMinutes = travelTime % 60;
+            travelTime = (travelTime - travelMinutes) / 60;
+            var travelHours = travelTime;
+            var arriveSeconds = (travelSeconds + startTime[2]) % 60;
+            var add = (travelSeconds + startTime[2]) / 60;
+            var arriveMinutes = (add + travelMinutes + startTime[1]) % 60;
+            add = (add + travelMinutes + startTime[1]) / 60;
+            var arriveHour = (add + travelHours + startTime[0]) % 24;
+
+            Console.WriteLine($"Time Arrival: {arriveHour:d2}:{arriveMinutes:d2}:{arriveSeconds:d2}");
+        }
+    }
+}
---
+++ L28_Exam Preparation I/E01_SinoTheWalker_v2/E01_SinoTheWalker_v2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace E01_SinoTheWalker_v2
+{
+    class E01_SinoTheWalker_v2
+    {
+        static void Main(string[] args)
+        {   // the good and still working verion
+            var timeFormat = "HH:mm:ss";
+            var timeString = Console.ReadLine();
+            var startTime = DateTime.ParseExact(
+                timeString, timeFormat, CultureInfo.InvariantCulture);
+            var stepsCount = long.Parse(Console.ReadLine());
+            var secondsPerStep = int.Parse(Console.ReadLine());
+            var travelTime = (stepsCount * secondsPerStep) % (3600 * 24);
+            var arriveTime = startTime.AddSeconds(travelTime);
+            Console.WriteLine($"Time Arrival: {arriveTime:HH:mm:ss}");
+        }
+    }
+}
