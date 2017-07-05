ceaf6d988a0fcc507d132c7fdda6256dd2d5eae5
Todor Chenkov <joto_to@mail.bg>
2017-07-05 10:30:03 +0300

    -add P05_OnlyLetters

---
+++ L27_StringsAndRegularExpressions-MoreExercises/P05_OnlyLetters/P05_OnlyLetters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P05_OnlyLetters
+{
+    class P05_OnlyLetters
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            var pattern = @"(?<digits>\d+)(?<ch>[A-Za-z])";
+            var matches = Regex.Matches(text, pattern);
+            foreach (Match match in matches)
+            {
+                text = text.Replace(
+                    match.Groups["digits"].Value, match.Groups["ch"].Value);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
