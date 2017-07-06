0de24d238359e5e56401c454589b1ea02add9343
Todor Chenkov <joto_to@mail.bg>
2017-07-06 09:42:47 +0300

    -add E01_CharityMarathon

---
+++ L29_Exam Preparation II/E01_CharityMarathon/E01_CharityMarathon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace E01_CharityMarathon
+{
+    class E01_CharityMarathon
+    {
+        static void Main(string[] args)
+        {
+            var durationDays = int.Parse(Console.ReadLine());
+            var runnersCount = int.Parse(Console.ReadLine());
+            var avLabsPerRunner = int.Parse(Console.ReadLine());
+            var trackLength = double.Parse(Console.ReadLine()); // meters
+            var trackCapacity = int.Parse(Console.ReadLine());
+            var moneyPerKm = double.Parse(Console.ReadLine()); // money per kilometer
+
+            var totalRunners = durationDays * trackCapacity < runnersCount ?
+                durationDays * trackCapacity :
+                runnersCount;
+
+            var totalMeters = totalRunners * avLabsPerRunner * trackLength;
+            var totalKilometers = totalMeters / 1000;
+            var moneyRaised = totalKilometers * moneyPerKm;
+
+            Console.WriteLine($"Money raised: {moneyRaised:f2}");
+        }
+    }
+}
