7da095a66f75201eb29ca72c5d752142795db3d0
Todor Chenkov <joto_to@mail.bg>
2017-07-07 16:46:58 +0300

    -add Exam Preparation IV conditions

---
+++ L30_Exam Preparation III/E03_RageQuit/E03_RageQuit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E03_RageQuit
+{
+    class E03_RageQuit
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
